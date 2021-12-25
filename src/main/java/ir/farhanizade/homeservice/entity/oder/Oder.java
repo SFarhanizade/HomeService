1f1790bfc5e59a27e93339be3d2348453cf4b3b6
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-25 15:43:48 +0330

    extends BaseEntity. @EqualsAndHashCode(callSuper = true), @Entity, @AllArgsConstructor, @NoArgsConstructor, @Data, @SuperBuilder added.

--- src/main/java/ir/farhanizade/homeservice/entity/oder/Oder.java
+++ src/main/java/ir/farhanizade/homeservice/entity/oder/Oder.java
@@ -1,4 +1,19 @@
 package ir.farhanizade.homeservice.entity.oder;
 
-public class Oder {
+import ir.farhanizade.homeservice.entity.core.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@SuperBuilder
+public class Oder extends BaseEntity {
 }
