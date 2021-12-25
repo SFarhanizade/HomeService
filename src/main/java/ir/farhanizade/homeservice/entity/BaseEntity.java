e9e7e18e3de87ce9f723b30081a747413e7128ed
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-25 13:14:21 +0330

    autogenerated id field added. @No/AllArgsConstructor added. @Data added. @SuperBuilder added.

--- src/main/java/ir/farhanizade/homeservice/entity/BaseEntity.java
+++ src/main/java/ir/farhanizade/homeservice/entity/BaseEntity.java
@@ -1,4 +1,23 @@
 package ir.farhanizade.homeservice.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@MappedSuperclass
 public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
