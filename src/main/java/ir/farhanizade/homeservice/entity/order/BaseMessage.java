595a7708ee08fed92e6151635f3f6f96d2eae855
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-25 21:57:44 +0330

    extends BaseEntity. @EqualsAndHashCode(callSuper = true), @AllArgsConstructor, @NoArgsConstructor, @Data, @SuperBuilder added.

--- src/main/java/ir/farhanizade/homeservice/entity/order/BaseMessage.java
+++ src/main/java/ir/farhanizade/homeservice/entity/order/BaseMessage.java
@@ -1,4 +1,34 @@
 package ir.farhanizade.homeservice.entity.order;
 
-public class BaseMessage {
+import ir.farhanizade.homeservice.entity.core.BaseEntity;
+import ir.farhanizade.homeservice.entity.user.User;
+import lombok.*;
+
+import javax.persistence.ManyToOne;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToOne;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@MappedSuperclass
+public class BaseMessage extends BaseEntity {
+
+    @OneToOne
+    private Order order;
+
+    @ManyToOne
+    private User owner;
+
+    private BigDecimal price;
+
+    private Date dateTime;
+
+    private Date suggestedDateTime;
+
+    @Builder.Default
+    private BaseMessageStatus status = BaseMessageStatus.HELD;
 }
