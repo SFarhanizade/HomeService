1f996334dd15ec99d73981122acec5783c9df74e
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-25 13:34:42 +0330

    extends BasePerson @EqualsAndHashCode(callSuper = true), @Entity, @AllArgsConstructor, @Data, and @SuperBuilder added.

--- src/main/java/ir/farhanizade/homeservice/User.java
+++ src/main/java/ir/farhanizade/homeservice/User.java
@@ -1,4 +1,17 @@
 package ir.farhanizade.homeservice;
 
-public class User {
+import ir.farhanizade.homeservice.entity.core.BasePerson;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@AllArgsConstructor
+@Data
+@SuperBuilder
+public class User extends BasePerson {
 }
