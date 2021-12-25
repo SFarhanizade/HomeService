cfd1f46f20129bcb857b111f9a454d4fbc653edf
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-25 22:18:13 +0330

    service, finishDateTime, request, suggestions, suggestion, and status fields and suggest method added.

--- src/main/java/ir/farhanizade/homeservice/entity/order/Order.java
+++ src/main/java/ir/farhanizade/homeservice/entity/order/Order.java
@@ -1,13 +1,18 @@
 package ir.farhanizade.homeservice.entity.order;
 
 import ir.farhanizade.homeservice.entity.core.BaseEntity;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import ir.farhanizade.homeservice.entity.service.SubService;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
@@ -16,4 +21,29 @@ import javax.persistence.Entity;
 @Data
 @SuperBuilder
 public class Order extends BaseEntity {
+
+    @ManyToOne
+    private SubService service;
+
+    private Date finishDateTime;
+
+    @OneToOne
+    private Request request;
+
+    @OneToMany
+    private List<Suggestion> suggestions;
+
+    @OneToOne
+    private Suggestion suggestion;
+
+    @Builder.Default
+    private OrderStatus status = OrderStatus.WAITING_FOR_SUGGESTION;
+
+    public void suggest(Suggestion suggestion){
+        if(suggestion==null)
+            throw new IllegalArgumentException("Null Suggestion!");
+        if(suggestions==null)
+            suggestions = new ArrayList<>();
+        suggestions.add(suggestion);
+    }
 }
