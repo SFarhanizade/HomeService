9f21ed6e3373d9a1e611f175257fda814207b9f1
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-25 14:18:14 +0330

    transactions added as a list.

--- src/main/java/ir/farhanizade/homeservice/entity/user/User.java
+++ src/main/java/ir/farhanizade/homeservice/entity/user/User.java
@@ -1,5 +1,6 @@
 package ir.farhanizade.homeservice.entity.user;
 
+import ir.farhanizade.homeservice.entity.Transaction;
 import ir.farhanizade.homeservice.entity.core.BasePerson;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -8,8 +9,10 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
@@ -21,4 +24,6 @@ public class User extends BasePerson {
     private Date dateTime;
     private BigDecimal credit;
     private UserStatus status;
+    @OneToMany
+    private List<Transaction> transactions;
 }
