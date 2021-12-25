187c53a7f6f6cb90cee085e12ae96808af5181ca
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-25 15:58:12 +0330

    extends BaseEntity. @EqualsAndHashCode(callSuper = true), @Entity, @AllArgsConstructor, @NoArgsConstructor, @Data, @SuperBuilder added.

--- src/main/java/ir/farhanizade/homeservice/entity/order/Suggestion.java
+++ src/main/java/ir/farhanizade/homeservice/entity/order/Suggestion.java
@@ -1,4 +1,19 @@
 package ir.farhanizade.homeservice.entity.order;
 
-public class Suggestion {
+import ir.farhanizade.homeservice.entity.core.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@SuperBuilder
+public class Suggestion extends BaseEntity {
 }
