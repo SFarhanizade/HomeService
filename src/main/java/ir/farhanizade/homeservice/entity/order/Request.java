0c627f0aff7ab5347884224b6b9e34714aa9a40a
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-25 22:17:03 +0330

    extends BaseMessage. @EqualsAndHashCode(callSuper = true), @Entity, @AllArgsConstructor, @NoArgsConstructor, @Data, @SuperBuilder added.

--- src/main/java/ir/farhanizade/homeservice/entity/order/Request.java
+++ src/main/java/ir/farhanizade/homeservice/entity/order/Request.java
@@ -1,4 +1,20 @@
 package ir.farhanizade.homeservice.entity.order;
 
-public class Request {
+import ir.farhanizade.homeservice.entity.user.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@SuperBuilder
+public class Request extends BaseMessage<Customer> {
+    private String address;
 }
