0808a30d171a224b4e342a5068e7802415e23184
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-25 13:30:16 +0330

    @NoArgsConstructor added.

--- src/main/java/ir/farhanizade/homeservice/entity/core/BasePerson.java
+++ src/main/java/ir/farhanizade/homeservice/entity/core/BasePerson.java
@@ -3,6 +3,7 @@ package ir.farhanizade.homeservice.entity.core;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.MappedSuperclass;
@@ -10,6 +11,7 @@ import javax.persistence.MappedSuperclass;
 @EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @SuperBuilder
 @MappedSuperclass
 public class BasePerson extends BaseEntity{
