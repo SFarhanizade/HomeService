ce9f39ae64678377900f61e5dd1ff573305d54d3
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-25 13:49:31 +0330

    extends BaseEntity. @Entity, @EqualsAndHashCode(callSuper = true), @Data, @AllArgsConstructor, @NoArgsConstructor, @SuperBuilder Added. @MappedSuperclass

--- src/main/java/ir/farhanizade/homeservice/entity/Transaction.java
+++ src/main/java/ir/farhanizade/homeservice/entity/Transaction.java
@@ -1,4 +1,20 @@
 package ir.farhanizade.homeservice.entity;
 
-public class Transaction {
+import ir.farhanizade.homeservice.entity.core.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.MappedSuperclass;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@MappedSuperclass
+public class Transaction extends BaseEntity {
+
 }
