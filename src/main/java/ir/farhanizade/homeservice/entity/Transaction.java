d4fa21fdc296ad8331cc4727c3dd015bde6e7571
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-25 13:53:12 +0330

    amount, payer, recipient, and dateTime fields added.

--- src/main/java/ir/farhanizade/homeservice/entity/Transaction.java
+++ src/main/java/ir/farhanizade/homeservice/entity/Transaction.java
@@ -1,13 +1,18 @@
 package ir.farhanizade.homeservice.entity;
 
 import ir.farhanizade.homeservice.entity.core.BaseEntity;
+import ir.farhanizade.homeservice.entity.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
 import javax.persistence.MappedSuperclass;
+import java.math.BigDecimal;
+import java.util.Date;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -16,5 +21,10 @@ import javax.persistence.MappedSuperclass;
 @SuperBuilder
 @MappedSuperclass
 public class Transaction extends BaseEntity {
-
+    private BigDecimal amount;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User payer;
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User recipient;
+    private Date dateTime;
 }
