c64ca6a1de90ff36dddde9fcbae4e4a558126a0c
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-25 22:16:24 +0330

    Expert typed passed to parent.

--- src/main/java/ir/farhanizade/homeservice/entity/order/Suggestion.java
+++ src/main/java/ir/farhanizade/homeservice/entity/order/Suggestion.java
@@ -6,9 +6,6 @@ import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import java.math.BigDecimal;
-import java.util.Date;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
@@ -16,7 +13,7 @@ import java.util.Date;
 @NoArgsConstructor
 @Data
 @SuperBuilder
-public class Suggestion extends BaseMessage {
+public class Suggestion extends BaseMessage<Expert> {
 
     private Double duration;
 
