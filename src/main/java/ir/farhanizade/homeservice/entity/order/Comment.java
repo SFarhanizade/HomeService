664bbdd912e09a2d804c279f2b71251d591ec8f9
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-25 22:35:31 +0330

    extends BaseEntity. @EqualsAndHashCode(callSuper = true), @Entity, @AllArgsConstructor, @NoArgsConstructor, @Data, @SuperBuilder added.

--- src/main/java/ir/farhanizade/homeservice/entity/order/Comment.java
+++ src/main/java/ir/farhanizade/homeservice/entity/order/Comment.java
@@ -1,4 +1,34 @@
 package ir.farhanizade.homeservice.entity.order;
 
-public class Comment {
+import ir.farhanizade.homeservice.entity.core.BaseEntity;
+import ir.farhanizade.homeservice.entity.user.Customer;
+import ir.farhanizade.homeservice.entity.user.Expert;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@SuperBuilder
+public class Comment extends BaseEntity {
+    private Integer points;
+    private String description;
+
+    @ManyToOne
+    private Customer sender;
+
+    @ManyToOne
+    private Expert recipient;
+
+    @OneToOne
+    private Order order;
 }
