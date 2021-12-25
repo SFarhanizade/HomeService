2a3e924ee7d8878460b64b13e182a2830c7d9857
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-25 21:44:41 +0330

    fields added.

--- src/main/java/ir/farhanizade/homeservice/entity/order/Suggestion.java
+++ src/main/java/ir/farhanizade/homeservice/entity/order/Suggestion.java
@@ -1,13 +1,14 @@
 package ir.farhanizade.homeservice.entity.order;
 
 import ir.farhanizade.homeservice.entity.core.BaseEntity;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import ir.farhanizade.homeservice.entity.user.Expert;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.util.Date;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
@@ -16,4 +17,18 @@ import javax.persistence.Entity;
 @Data
 @SuperBuilder
 public class Suggestion extends BaseEntity {
+    @ManyToOne
+    private Order order;
+
+    @ManyToOne
+    private Expert expert;
+
+    private Date dateTime;
+    private BigDecimal price;
+
+    private Double duration;
+    private Date startDateTime;
+
+    @Builder.Default
+    private SuggestionStatus status = SuggestionStatus.PENDING;
 }
