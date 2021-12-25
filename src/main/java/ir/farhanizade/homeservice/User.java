5515f411d418e4acf424ee8c7c729a0f2a0a7c26
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-25 13:38:21 +0330

    dateTime and credit fields added. @NoArgsConstructor added.

--- src/main/java/ir/farhanizade/homeservice/User.java
+++ src/main/java/ir/farhanizade/homeservice/User.java
@@ -4,14 +4,20 @@ import ir.farhanizade.homeservice.entity.core.BasePerson;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
+import java.math.BigDecimal;
+import java.util.Date;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @SuperBuilder
 public class User extends BasePerson {
+    private Date dateTime;
+    private BigDecimal credit;
 }
