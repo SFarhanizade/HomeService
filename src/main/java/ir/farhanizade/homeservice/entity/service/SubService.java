950e58c26c1fc2826e37fa3baf092afde5eaba76
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-25 15:38:16 +0330

    extends BaseService. @EqualsAndHashCode(callSuper = true), @Entity, @AllArgsConstructor, @NoArgsConstructor, @Data, @SuperBuilder added.

--- src/main/java/ir/farhanizade/homeservice/entity/service/SubService.java
+++ src/main/java/ir/farhanizade/homeservice/entity/service/SubService.java
@@ -1,4 +1,20 @@
 package ir.farhanizade.homeservice.entity.service;
 
-public class SubService {
+import ir.farhanizade.homeservice.entity.core.BaseService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@SuperBuilder
+public class SubService extends BaseService {
+
 }
