0acb05b53c73aa0a915e01451cb7d7ee7d20d8b7
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-26 20:19:07 +0330

    Initial Commit

---
+++ src/main/java/ir/farhanizade/homeservice/exception/UserNotValidException.java
@@ -0,0 +1,7 @@
+package ir.farhanizade.homeservice.exception;
+
+public class UserNotValidException extends Exception {
+    public UserNotValidException(String message) {
+        super(message);
+    }
+}
