1b423f84203da54f11f996b37839d053104ae630
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-26 19:28:03 +0330

    findByEmail,Credit,Status methods added.

--- src/main/java/ir/farhanizade/homeservice/repository/user/CustomerRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/user/CustomerRepository.java
@@ -3,5 +3,11 @@ package ir.farhanizade.homeservice.repository.user;
 import ir.farhanizade.homeservice.entity.user.Customer;
 import ir.farhanizade.homeservice.repository.BaseRepository;
 
+import java.math.BigDecimal;
+
 public interface CustomerRepository extends BaseRepository<Customer> {
+    Customer findByEmail(String email);
+    Customer findByCredit(BigDecimal credit);
+    Customer findByStatus(String status);
+
 }
