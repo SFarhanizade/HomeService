630ea40209803964fc48edd4479145db671f530a
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-26 21:04:53 +0330

    findByExpertise method added.

--- src/main/java/ir/farhanizade/homeservice/repository/user/ExpertRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/user/ExpertRepository.java
@@ -5,13 +5,17 @@ import ir.farhanizade.homeservice.entity.user.Customer;
 import ir.farhanizade.homeservice.entity.user.Expert;
 import ir.farhanizade.homeservice.entity.user.UserStatus;
 import ir.farhanizade.homeservice.repository.BaseRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 
 public interface ExpertRepository extends BaseRepository<Expert> {
     Expert findByEmail(String email);
     Expert findByCredit(BigDecimal credit);
     Expert findByStatus(UserStatus status);
-    Expert findByExpertise(SubService service);
+
+    @Query("from Expert e where :service in e.expertises")
+    List<Expert> findByExpertise(SubService service);
 }
