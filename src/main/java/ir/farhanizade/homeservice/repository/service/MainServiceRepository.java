c2bb16210dc6bcad2e8ed0d7eb0f5a3627dadc32
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-26 19:02:13 +0330

    findByName method added.

--- src/main/java/ir/farhanizade/homeservice/repository/service/MainServiceRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/service/MainServiceRepository.java
@@ -4,4 +4,5 @@ import ir.farhanizade.homeservice.entity.service.MainService;
 import ir.farhanizade.homeservice.repository.BaseRepository;
 
 public interface MainServiceRepository extends BaseRepository<MainService> {
+    MainService findByName(String name);
 }
