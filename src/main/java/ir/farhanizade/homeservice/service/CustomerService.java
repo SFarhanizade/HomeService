4210ba5a6a29a641ad4a701f72e770c7c75ad23e
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-26 19:40:27 +0330

    findByCredit,Status methods added.

--- src/main/java/ir/farhanizade/homeservice/service/CustomerService.java
+++ src/main/java/ir/farhanizade/homeservice/service/CustomerService.java
@@ -1,10 +1,13 @@
 package ir.farhanizade.homeservice.service;
 
 import ir.farhanizade.homeservice.entity.user.Customer;
+import ir.farhanizade.homeservice.entity.user.UserStatus;
 import ir.farhanizade.homeservice.repository.user.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 @Service
 @RequiredArgsConstructor
@@ -12,11 +15,19 @@ public class CustomerService {
     private final CustomerRepository repository;
 
     @Transactional
-    public void save(Customer customer){
+    public void save(Customer customer) {
         repository.save(customer);
     }
 
-    public Customer findByEmail(String email){
+    public Customer findByEmail(String email) {
         return repository.findByEmail(email);
+    }
+
+    public Customer findByCredit(BigDecimal credit) {
+        return repository.findByCredit(credit);
+    }
+
+    public Customer findByStatus(UserStatus status) {
+        return repository.findByStatus(status);
     }
 }
