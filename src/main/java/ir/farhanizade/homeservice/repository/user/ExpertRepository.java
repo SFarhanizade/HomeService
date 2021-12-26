112890a75245a050daa2554513e15903d10a8bde
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-26 20:52:48 +0330

    findByEmail,Credit,Status,Expertise methods added.

--- src/main/java/ir/farhanizade/homeservice/repository/user/ExpertRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/user/ExpertRepository.java
@@ -1,7 +1,17 @@
 package ir.farhanizade.homeservice.repository.user;
 
+import ir.farhanizade.homeservice.entity.service.SubService;
+import ir.farhanizade.homeservice.entity.user.Customer;
 import ir.farhanizade.homeservice.entity.user.Expert;
+import ir.farhanizade.homeservice.entity.user.UserStatus;
 import ir.farhanizade.homeservice.repository.BaseRepository;
 
+import java.math.BigDecimal;
+
+
 public interface ExpertRepository extends BaseRepository<Expert> {
+    Expert findByEmail(String email);
+    Expert findByCredit(BigDecimal credit);
+    Expert findByStatus(UserStatus status);
+    Expert findByExpertise(SubService service);
 }
