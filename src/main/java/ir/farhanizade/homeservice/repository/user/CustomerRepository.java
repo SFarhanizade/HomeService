49f390017fb79bcea434b335a903cac22ba40cf1
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-26 19:39:42 +0330

    findByStatus parameter type changed to UserStatus.

--- src/main/java/ir/farhanizade/homeservice/repository/user/CustomerRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/user/CustomerRepository.java
@@ -1,6 +1,7 @@
 package ir.farhanizade.homeservice.repository.user;
 
 import ir.farhanizade.homeservice.entity.user.Customer;
+import ir.farhanizade.homeservice.entity.user.UserStatus;
 import ir.farhanizade.homeservice.repository.BaseRepository;
 
 import java.math.BigDecimal;
@@ -8,6 +9,6 @@ import java.math.BigDecimal;
 public interface CustomerRepository extends BaseRepository<Customer> {
     Customer findByEmail(String email);
     Customer findByCredit(BigDecimal credit);
-    Customer findByStatus(String status);
+    Customer findByStatus(UserStatus status);
 
 }
