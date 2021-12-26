e8ec30f18f51cf0371c3ead58e9a0182ecfaf551
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-26 19:03:37 +0330

    generic type name changed.

--- src/main/java/ir/farhanizade/homeservice/repository/order/OrderRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/OrderRepository.java
@@ -1,7 +1,7 @@
 package ir.farhanizade.homeservice.repository.order;
 
-import ir.farhanizade.homeservice.entity.order.Order;
+import ir.farhanizade.homeservice.entity.order.ServiceOrder;
 import ir.farhanizade.homeservice.repository.BaseRepository;
 
-public interface OrderRepository extends BaseRepository<Order> {
+public interface OrderRepository extends BaseRepository<ServiceOrder> {
 }
