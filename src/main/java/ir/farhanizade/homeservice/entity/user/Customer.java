ba7fe6b7becb618bfcb162b22aaf09cfe488364b
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-26 20:45:15 +0330

    orders list added.

--- src/main/java/ir/farhanizade/homeservice/entity/user/Customer.java
+++ src/main/java/ir/farhanizade/homeservice/entity/user/Customer.java
@@ -1,22 +1,26 @@
 package ir.farhanizade.homeservice.entity.user;
 
+import ir.farhanizade.homeservice.entity.order.ServiceOrder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
 
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @AllArgsConstructor
-//@NoArgsConstructor
+@NoArgsConstructor
 @Data
 @SuperBuilder
 public class Customer extends User {
 
-    /*@OneToMany
-    private List<Order> orders;*/
+    @OneToMany
+    private List<ServiceOrder> orders;
 
 
 }
