c71965acd963e16cb653a51c337940c27517b813
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-27 13:37:06 +0330

    findByStatus method added.

--- src/main/java/ir/farhanizade/homeservice/repository/order/message/RequestRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/message/RequestRepository.java
@@ -1,7 +1,11 @@
 package ir.farhanizade.homeservice.repository.order.message;
 
+import ir.farhanizade.homeservice.entity.order.message.BaseMessageStatus;
 import ir.farhanizade.homeservice.entity.order.message.Request;
 import ir.farhanizade.homeservice.repository.BaseRepository;
 
+import java.util.List;
+
 public interface RequestRepository extends BaseRepository<Request> {
+    List<Request> findByStatus(BaseMessageStatus waiting);
 }
