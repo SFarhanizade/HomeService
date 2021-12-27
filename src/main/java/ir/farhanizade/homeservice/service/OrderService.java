9aebba0f3e054c0132bdf80ff71104b80931e8df
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-27 12:59:28 +0330

    save method added.

--- src/main/java/ir/farhanizade/homeservice/service/OrderService.java
+++ src/main/java/ir/farhanizade/homeservice/service/OrderService.java
@@ -1,4 +1,16 @@
 package ir.farhanizade.homeservice.service;
 
+import ir.farhanizade.homeservice.entity.order.ServiceOrder;
+import ir.farhanizade.homeservice.repository.order.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class OrderService {
+    private final OrderRepository repository;
+
+    public void save(ServiceOrder order){
+        repository.save(order);
+    }
 }
