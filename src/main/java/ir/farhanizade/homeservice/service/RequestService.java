abbc9093e1d40c7701eb0005833ebab9321be914
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-27 13:37:12 +0330

    save, loadAll, loadWaitingRequests methods added.

--- src/main/java/ir/farhanizade/homeservice/service/RequestService.java
+++ src/main/java/ir/farhanizade/homeservice/service/RequestService.java
@@ -1,4 +1,27 @@
 package ir.farhanizade.homeservice.service;
 
+import ir.farhanizade.homeservice.entity.order.message.BaseMessageStatus;
+import ir.farhanizade.homeservice.entity.order.message.Request;
+import ir.farhanizade.homeservice.repository.order.message.RequestRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class RequestService {
+    private final RequestRepository repository;
+
+    public void save(Request request){
+        repository.save(request);
+    }
+
+    public List<Request> loadAll(){
+        return repository.findAll();
+    }
+
+    public List<Request> loadWaitingRequests(){
+        return repository.findByStatus(BaseMessageStatus.WAITING);
+    }
 }
