415668b8fe2a67001935fe54ac232d84f2acecfa
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-27 14:48:58 +0330

    save and loadAll method added.

--- src/main/java/ir/farhanizade/homeservice/service/SuggestionService.java
+++ src/main/java/ir/farhanizade/homeservice/service/SuggestionService.java
@@ -1,4 +1,21 @@
 package ir.farhanizade.homeservice.service;
 
+import ir.farhanizade.homeservice.entity.order.message.Suggestion;
+import ir.farhanizade.homeservice.repository.order.message.SuggestionRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class SuggestionService {
+    private final SuggestionRepository repository;
+    public void save(Suggestion suggestion) {
+        repository.save(suggestion);
+    }
+
+    public List<Suggestion> loadAll(){
+        return repository.findAll();
+    }
 }
