272f2997dda6978305166c46d1ac7cba21b0126e
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-27 17:46:12 +0330

    payer and recipient type changed to their real type.

--- src/main/java/ir/farhanizade/homeservice/entity/Transaction.java
+++ src/main/java/ir/farhanizade/homeservice/entity/Transaction.java
@@ -1,14 +1,13 @@
 package ir.farhanizade.homeservice.entity;
 
 import ir.farhanizade.homeservice.entity.core.BaseEntity;
+import ir.farhanizade.homeservice.entity.user.Customer;
+import ir.farhanizade.homeservice.entity.user.Expert;
 import ir.farhanizade.homeservice.entity.user.User;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.util.Date;
 
@@ -22,9 +21,9 @@ import java.util.Date;
 @SuperBuilder
 public class Transaction extends BaseEntity {
     private BigDecimal amount;
-    @ManyToOne(fetch = FetchType.EAGER)
-    private User payer;
-    @ManyToOne(fetch = FetchType.EAGER)
-    private User recipient;
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Customer payer;
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Expert recipient;
     private Date dateTime;
 }
