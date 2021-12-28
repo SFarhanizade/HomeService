dc075e8e48dc213fa94124629a9c42caa8382061
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-28 15:05:00 +0330

    save method added.

--- src/main/java/ir/farhanizade/homeservice/service/CommentSevice.java
+++ src/main/java/ir/farhanizade/homeservice/service/CommentSevice.java
@@ -1,4 +1,15 @@
 package ir.farhanizade.homeservice.service;
 
+import ir.farhanizade.homeservice.entity.order.Comment;
+import ir.farhanizade.homeservice.repository.order.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class CommentSevice {
+    private final CommentRepository repository;
+    public void save(Comment comment){
+        repository.save(comment);
+    }
 }
