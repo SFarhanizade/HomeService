c32975e1cdcc934e70a208cfc2e6508ca607c6eb
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2021-12-31 10:32:48 +0330

    change UserType to Class.

--- src/main/java/ir/farhanizade/homeservice/entity/user/UserType.java
+++ src/main/java/ir/farhanizade/homeservice/entity/user/UserType.java
@@ -1,5 +1,19 @@
 package ir.farhanizade.homeservice.entity.user;
 
-public enum UserType {
-    ADMIN, CUSTOMER, EXPERT
+import ir.farhanizade.homeservice.entity.core.BaseEntity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+@Entity
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class UserType extends BaseEntity {
+    private String role;
 }
