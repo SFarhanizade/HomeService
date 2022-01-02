b68cc00a2d864fb3862fa12cb9ddd3b6f80fd7f5
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-02 17:29:48 +0330

    loadByExpertises method added.

--- src/main/java/ir/farhanizade/homeservice/repository/order/OrderRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/OrderRepository.java
@@ -1,7 +1,15 @@
 package ir.farhanizade.homeservice.repository.order;
 
 import ir.farhanizade.homeservice.entity.order.ServiceOrder;
+import ir.farhanizade.homeservice.entity.service.SubService;
 import ir.farhanizade.homeservice.repository.BaseRepository;
+import ir.farhanizade.homeservice.service.OrderService;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface OrderRepository extends BaseRepository<ServiceOrder> {
+
+    @Query("From ServiceOrder o where o.service in :expertises")
+    List<OrderService> loadByExpertises(List<SubService> expertises);
 }
