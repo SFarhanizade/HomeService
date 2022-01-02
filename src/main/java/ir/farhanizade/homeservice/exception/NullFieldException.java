d577474d571e63b925f35f3fae134d7d498553ec
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-02 16:19:17 +0330

    Initial Commit

---
+++ src/main/java/ir/farhanizade/homeservice/exception/NullFieldException.java
@@ -0,0 +1,7 @@
+package ir.farhanizade.homeservice.exception;
+
+public class NullFieldException extends Exception {
+    public NullFieldException(String message) {
+        super(message);
+    }
+}
