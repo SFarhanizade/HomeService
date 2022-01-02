e3b0f5c94f3b2a8fefc64a92f7ea446ce97a99fb
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-02 16:22:17 +0330

    Initial Commit

---
+++ src/main/java/ir/farhanizade/homeservice/exception/BadPriceException.java
@@ -0,0 +1,7 @@
+package ir.farhanizade.homeservice.exception;
+
+public class BadPriceException extends Exception {
+    public BadPriceException(String message) {
+        super(message);
+    }
+}
