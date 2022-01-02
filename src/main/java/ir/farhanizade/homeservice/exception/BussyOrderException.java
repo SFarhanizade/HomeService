a6ab170fa6a5ef1f9047f2c9770bff075c5ac533
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-02 17:23:37 +0330

    Initial Commit.

---
+++ src/main/java/ir/farhanizade/homeservice/exception/BussyOrderException.java
@@ -0,0 +1,7 @@
+package ir.farhanizade.homeservice.exception;
+
+public class BussyOrderException extends Exception {
+    public BussyOrderException(String message) {
+        super(message);
+    }
+}
