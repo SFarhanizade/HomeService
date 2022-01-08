79a6a19aa5ff6acb3e860325ca8ecac7082e82d2
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-08 17:25:39 +0330

    add changePassword method and validation to UserService and UserRepository

--- src/main/java/ir/farhanizade/homeservice/repository/user/UserRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/user/UserRepository.java
@@ -2,8 +2,15 @@ package ir.farhanizade.homeservice.repository.user;
 
 import ir.farhanizade.homeservice.entity.user.User;
 import ir.farhanizade.homeservice.repository.BaseRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends BaseRepository<User> {
 
+    @Query("From User u where u.id=:id and u.password=:currentPassword")
+    User isCorrectByPassword(Long id, String currentPassword);
 
+    @Modifying
+    @Query("Update User u set u.password=:newPassword where u.id=:id")
+    User updatePassword(Long id, String newPassword);
 }
--- src/main/java/ir/farhanizade/homeservice/service/UserService.java
+++ src/main/java/ir/farhanizade/homeservice/service/UserService.java
@@ -1,11 +1,33 @@
 package ir.farhanizade.homeservice.service;
 
 
+import ir.farhanizade.homeservice.entity.user.User;
+import ir.farhanizade.homeservice.exception.PasswordNotValidException;
+import ir.farhanizade.homeservice.exception.WrongPasswordException;
+import ir.farhanizade.homeservice.repository.user.UserRepository;
+import ir.farhanizade.homeservice.service.util.Validation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
+    private final UserRepository repository;
 
+    @Transactional
+    public User changePassword(Long id, String currentPassword, String newPassword) throws PasswordNotValidException, WrongPasswordException {
+        if(currentPassword.equals(newPassword)){
+            throw new PasswordNotValidException("The new password is the same as the current password!");
+        }
+        if(!Validation.passwordIsValid(newPassword)){
+            throw new PasswordNotValidException("The new password is not valid!");
+        }
+        User byIdAndPass = repository.isCorrectByPassword(id, currentPassword);
+        if(!currentPassword.equals(byIdAndPass.getPassword())){
+            throw new WrongPasswordException("The current password is not correct!");
+        }
+        User result = repository.updatePassword(id, newPassword);
+        return result;
+    }
 }
