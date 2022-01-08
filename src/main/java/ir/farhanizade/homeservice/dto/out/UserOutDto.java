d2c123fe4fd506f9471a22213bfa83b441b3d12a
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-08 15:51:28 +0330

    All/NoArgsConstructor added to UserOutDto

--- src/main/java/ir/farhanizade/homeservice/dto/out/UserOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/UserOutDto.java
@@ -1,8 +1,12 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserOutDto {
     private String type;
     private Long id;
