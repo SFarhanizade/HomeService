00829e130176f344145bcfdc47c759b10bf2e132
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-08 23:17:19 +0330

    add convert2Dto method to UserSearchOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/UserSearchOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/UserSearchOutDto.java
@@ -1,11 +1,24 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import ir.farhanizade.homeservice.entity.user.User;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class UserSearchOutDto {
+    protected Long id;
     protected String type;
     protected String firstname;
     protected String lastname;
     protected String email;
+
+    public UserSearchOutDto convert2Dto(User user){
+        return UserSearchOutDto.builder()
+                .id(user.getId())
+                .firstname(user.getFName())
+                .lastname(user.getLName())
+                .email(user.getEmail())
+                .build();
+    }
 }
