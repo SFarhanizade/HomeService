b9c58f57940a2f1376f1410fc21c49030c85fac9
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-08 17:30:40 +0330

    add required fields to UserPasswordInDto

--- src/main/java/ir/farhanizade/homeservice/dto/in/UserPasswordInDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/in/UserPasswordInDto.java
@@ -1,4 +1,12 @@
 package ir.farhanizade.homeservice.dto.in;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class UserPasswordInDto {
+    private Long id;
+    private String currentPassword;
+    private String newPassword;
 }
