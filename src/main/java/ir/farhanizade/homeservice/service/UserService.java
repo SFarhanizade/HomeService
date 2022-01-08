447333881bde46fa8af1bbfaca8898e129753366
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-08 17:24:17 +0330

    create UserService class in service package

---
+++ src/main/java/ir/farhanizade/homeservice/service/UserService.java
@@ -0,0 +1,11 @@
+package ir.farhanizade.homeservice.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+}
