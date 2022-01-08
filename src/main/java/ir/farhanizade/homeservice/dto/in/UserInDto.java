031df9f350cf741c0fdd59ef7d2189d6b08a65f3
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-08 15:40:03 +0330

    required fields added to UserInDto

--- src/main/java/ir/farhanizade/homeservice/dto/in/UserInDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/in/UserInDto.java
@@ -1,4 +1,22 @@
 package ir.farhanizade.homeservice.dto.in;
 
+import ir.farhanizade.homeservice.entity.user.User;
+import lombok.Data;
+
+@Data
 public class UserInDto {
+    private String type;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+
+    public User convert() {
+        return User.builder()
+                .fName(firstname)
+                .lName(lastname)
+                .email(email)
+                .password(password)
+                .build();
+    }
 }
