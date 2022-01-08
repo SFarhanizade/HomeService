e4804b10582a25302180691e94a6cdab8b047f98
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-08 17:55:17 +0330

    solve NullPointerException in changePassword method in UserService class

--- src/main/java/ir/farhanizade/homeservice/service/UserService.java
+++ src/main/java/ir/farhanizade/homeservice/service/UserService.java
@@ -16,7 +16,7 @@ public class UserService {
     private final UserRepository repository;
 
     @Transactional
-    public User changePassword(Long id, String currentPassword, String newPassword) throws PasswordNotValidException, WrongPasswordException {
+    public int changePassword(Long id, String currentPassword, String newPassword) throws PasswordNotValidException, WrongPasswordException {
         if(currentPassword.equals(newPassword)){
             throw new PasswordNotValidException("The new password is the same as the current password!");
         }
@@ -24,10 +24,12 @@ public class UserService {
             throw new PasswordNotValidException("The new password is not valid!");
         }
         User byIdAndPass = repository.isCorrectByPassword(id, currentPassword);
-        if(!currentPassword.equals(byIdAndPass.getPassword())){
+        if(byIdAndPass==null){
             throw new WrongPasswordException("The current password is not correct!");
         }
-        User result = repository.updatePassword(id, newPassword);
+        int result = repository.updatePassword(id, newPassword);
+
+
         return result;
     }
 }
