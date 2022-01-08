e813e0851b69f14d84220eb3470b7bcc6453b664
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-08 16:23:37 +0330

    No/AllArgsConstructor added to ResponseResult class

--- src/main/java/ir/farhanizade/homeservice/controller/api/ResponseResult.java
+++ src/main/java/ir/farhanizade/homeservice/controller/api/ResponseResult.java
@@ -1,10 +1,14 @@
 package ir.farhanizade.homeservice.controller.api;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ResponseResult<T> {
     private int code;
     private T data;
