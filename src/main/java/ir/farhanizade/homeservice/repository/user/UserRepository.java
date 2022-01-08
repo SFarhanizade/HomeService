673c785e6fc4abe54b45489d0e569c41e94575ee
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-08 17:23:12 +0330

    create UserRepository interface in repository.user package

---
+++ src/main/java/ir/farhanizade/homeservice/repository/user/UserRepository.java
@@ -0,0 +1,9 @@
+package ir.farhanizade.homeservice.repository.user;
+
+import ir.farhanizade.homeservice.entity.user.User;
+import ir.farhanizade.homeservice.repository.BaseRepository;
+
+public interface UserRepository extends BaseRepository<User> {
+
+
+}
