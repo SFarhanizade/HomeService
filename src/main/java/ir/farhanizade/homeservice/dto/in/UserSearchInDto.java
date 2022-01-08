48cfe0431d6df3b55652e36a5f0f96e469fd956d
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-08 23:30:08 +0330

    add required fields to UserSearchInDto class

--- src/main/java/ir/farhanizade/homeservice/dto/in/UserSearchInDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/in/UserSearchInDto.java
@@ -1,4 +1,15 @@
 package ir.farhanizade.homeservice.dto.in;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class UserSearchInDto {
+    protected String type;
+    protected String firstname;
+    protected String lastname;
+    protected String email;
+    private List<Long> expertises;
+    private int points;
 }
