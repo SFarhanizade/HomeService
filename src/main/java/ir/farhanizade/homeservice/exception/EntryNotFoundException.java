7f7b32219dad6bbd3a5f0ecfd4f1e6ff42f73454
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-09 14:52:37 +0330

    create EntityNotFoundException class in exception package

---
+++ src/main/java/ir/farhanizade/homeservice/exception/EntryNotFoundException.java
@@ -0,0 +1,7 @@
+package ir.farhanizade.homeservice.exception;
+
+public class EntryNotFoundException extends Exception {
+    public EntryNotFoundException(String message) {
+        super(message);
+    }
+}
