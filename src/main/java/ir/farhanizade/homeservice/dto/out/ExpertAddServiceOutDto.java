20c5e00fa5111a5c6f75e36b2dd5698173832b0b
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-09 19:43:52 +0330

    add required fields to ExpertAddServiceOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/ExpertAddServiceOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/ExpertAddServiceOutDto.java
@@ -1,4 +1,15 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class ExpertAddServiceOutDto {
+    private Long expertId;
+    private List<EntityOutDto> services;
 }
