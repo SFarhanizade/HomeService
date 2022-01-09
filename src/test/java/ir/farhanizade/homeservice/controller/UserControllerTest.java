64a1897a68a322516aeabe3ef6340b61fb4cbc23
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-09 16:55:07 +0330

    create UserControllerTest class in test package

---
+++ src/test/java/ir/farhanizade/homeservice/controller/UserControllerTest.java
@@ -0,0 +1,16 @@
+package ir.farhanizade.homeservice.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void changePassword() {
+    }
+}
