f6fd5318fedf7baa3630180b2ad459658acaa08b
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-09 14:49:23 +0330

    add save method to ServiceController class

--- src/main/java/ir/farhanizade/homeservice/controller/ServiceController.java
+++ src/main/java/ir/farhanizade/homeservice/controller/ServiceController.java
@@ -1,17 +1,19 @@
 package ir.farhanizade.homeservice.controller;
 
 import ir.farhanizade.homeservice.controller.api.ResponseResult;
+import ir.farhanizade.homeservice.dto.in.ServiceInDto;
+import ir.farhanizade.homeservice.dto.out.EntityOutDto;
 import ir.farhanizade.homeservice.dto.out.MainServiceOutDto;
 import ir.farhanizade.homeservice.dto.out.ServiceOutDto;
+import ir.farhanizade.homeservice.entity.core.BaseEntity;
 import ir.farhanizade.homeservice.entity.service.MainService;
+import ir.farhanizade.homeservice.exception.DuplicateEntityException;
 import ir.farhanizade.homeservice.service.MainServiceService;
 import ir.farhanizade.homeservice.service.SubServiceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -42,5 +44,30 @@ public class ServiceController {
                         .build()).toList();
         response.setData(dtos);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseResult<EntityOutDto>> save(@RequestBody ServiceInDto service) {
+        ResponseResult<EntityOutDto> response = ResponseResult.<EntityOutDto>builder()
+                .code(1)
+                .message("add successfully!")
+                .build();
+        BaseEntity result = new BaseEntity();
+
+            try {
+                if (service.getParent() == 0) {
+                    result = mainService.save(service.convert2MainService());
+                    response.setMessage("MainService "+response.getMessage());
+                } else {
+                    result = subService.save(service.convert2SubService(), service.getParent());
+                    response.setMessage("SubService "+response.getMessage());
+                }
+            } catch (DuplicateEntityException e) {
+                e.printStackTrace();
+                response.setMessage(e.getMessage());
+                response.setCode(-1);
+            }
+            response.setData(new EntityOutDto(result.getId()));
+            return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
