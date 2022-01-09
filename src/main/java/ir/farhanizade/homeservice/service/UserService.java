21270f2c39a0d7479490c59e9184f667950af924
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-09 18:39:14 +0330

    add save method to UserService class

--- src/main/java/ir/farhanizade/homeservice/service/UserService.java
+++ src/main/java/ir/farhanizade/homeservice/service/UserService.java
@@ -1,11 +1,11 @@
 package ir.farhanizade.homeservice.service;
 
 
+import ir.farhanizade.homeservice.dto.in.UserInDto;
 import ir.farhanizade.homeservice.dto.in.UserPasswordInDto;
 import ir.farhanizade.homeservice.dto.out.EntityOutDto;
 import ir.farhanizade.homeservice.entity.user.User;
-import ir.farhanizade.homeservice.exception.PasswordNotValidException;
-import ir.farhanizade.homeservice.exception.WrongPasswordException;
+import ir.farhanizade.homeservice.exception.*;
 import ir.farhanizade.homeservice.repository.user.UserRepository;
 import ir.farhanizade.homeservice.service.util.Validation;
 import lombok.RequiredArgsConstructor;
@@ -16,25 +16,38 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository repository;
+    private final ExpertService expertService;
+    private final CustomerService customerService;
 
     @Transactional
     public EntityOutDto changePassword(UserPasswordInDto user) throws PasswordNotValidException, WrongPasswordException {
         String currentPassword = user.getCurrentPassword();
         String newPassword = user.getNewPassword();
         Long id = user.getId();
-        if(currentPassword.equals(newPassword)){
+        if (currentPassword.equals(newPassword)) {
             throw new PasswordNotValidException("The new password is the same as the current password!");
         }
-        if(!Validation.passwordIsValid(newPassword)){
+        if (!Validation.passwordIsValid(newPassword)) {
             throw new PasswordNotValidException("The new password is not valid!");
         }
         User byIdAndPass = repository.isCorrectByPassword(id, currentPassword);
-        if(byIdAndPass==null){
+        if (byIdAndPass == null) {
             throw new WrongPasswordException("The current password is not correct!");
         }
         repository.updatePassword(id, newPassword);
 
 
         return new EntityOutDto(id);
+    }
+
+    public EntityOutDto save(UserInDto user) throws DuplicateEntityException, NameNotValidException, EmailNotValidException, PasswordNotValidException, UserNotValidException, NullFieldException {
+        EntityOutDto result = new EntityOutDto();
+        if ("expert".equals(user.getType())) {
+            result = expertService.save(user);
+
+        } else if ("customer".equals(user.getType())) {
+            result = customerService.save(user);
+        }
+        return result;
     }
 }
