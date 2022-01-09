d885eafb915eb3510bdd8849334307400b0e58bf
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-09 14:47:35 +0330

    add description and basePrice to ServiceInDto class

--- src/main/java/ir/farhanizade/homeservice/dto/in/ServiceInDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/in/ServiceInDto.java
@@ -1,9 +1,27 @@
 package ir.farhanizade.homeservice.dto.in;
 
+import ir.farhanizade.homeservice.entity.service.MainService;
+import ir.farhanizade.homeservice.entity.service.SubService;
 import lombok.Data;
+
+import java.math.BigDecimal;
 
 @Data
 public class ServiceInDto {
     private Long parent;
     private String name;
+    private String description;
+    private Long basePrice;
+
+    public MainService convert2MainService(){
+        return MainService.builder().name(name).build();
+    }
+
+    public SubService convert2SubService(){
+        return SubService.builder()
+                .name(name)
+                .description(description)
+                .basePrice(new BigDecimal(basePrice))
+                .build();
+    }
 }
