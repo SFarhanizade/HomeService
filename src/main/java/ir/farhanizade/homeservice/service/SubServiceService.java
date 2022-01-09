554085302291518bb95e6cb04f88427d84a034fc
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-09 15:08:48 +0330

    add some validation to save method in SubServiceService class

--- src/main/java/ir/farhanizade/homeservice/service/SubServiceService.java
+++ src/main/java/ir/farhanizade/homeservice/service/SubServiceService.java
@@ -3,14 +3,13 @@ package ir.farhanizade.homeservice.service;
 import ir.farhanizade.homeservice.entity.service.MainService;
 import ir.farhanizade.homeservice.entity.service.SubService;
 import ir.farhanizade.homeservice.exception.DuplicateEntityException;
+import ir.farhanizade.homeservice.exception.EntityNotFoundException;
 import ir.farhanizade.homeservice.repository.service.MainServiceRepository;
 import ir.farhanizade.homeservice.repository.service.SubServiceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,10 +18,10 @@ public class SubServiceService {
     private final MainServiceRepository parentRepository;
 
     @Transactional
-    public SubService save(SubService entity, Long parentId) throws DuplicateEntityException {
+    public SubService save(SubService entity, Long parentId) throws DuplicateEntityException, EntityNotFoundException {
         MainService parent = parentRepository.getById(parentId);
         if (parent == null)
-            throw new IllegalStateException();
+            throw new EntityNotFoundException("Parent Is Not Found!");
         entity.setParent(parent);
         List<SubService> siblings = parent.getSubServices();
         boolean noneMatch = true;
