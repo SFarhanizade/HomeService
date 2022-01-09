14660bd17c9bb65d79e3597bcb0d1b5f0991ccd8
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-09 15:09:11 +0330

    add some validation to save and loadAll methods in MainServiceService class

--- src/main/java/ir/farhanizade/homeservice/service/MainServiceService.java
+++ src/main/java/ir/farhanizade/homeservice/service/MainServiceService.java
@@ -2,11 +2,12 @@ package ir.farhanizade.homeservice.service;
 
 import ir.farhanizade.homeservice.entity.service.MainService;
 import ir.farhanizade.homeservice.exception.DuplicateEntityException;
+import ir.farhanizade.homeservice.exception.EntityNotFoundException;
+import ir.farhanizade.homeservice.exception.NullFieldException;
 import ir.farhanizade.homeservice.repository.service.MainServiceRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 
 @Service
@@ -15,13 +16,18 @@ public class MainServiceService {
     private final MainServiceRepository repository;
 
     @Transactional
-    public MainService save(MainService entity) throws DuplicateEntityException {
+    public MainService save(MainService entity) throws DuplicateEntityException, NullFieldException {
+        if(entity.getName()==null) throw new NullFieldException("Name is null!");
+        if("".equals(entity.getName())) throw new NullFieldException("Name is empty!");
         MainService result = repository.findByName(entity.getName());
-        if (result != null && entity.getId()==null) throw new DuplicateEntityException("");
+        if (result != null && entity.getId()==null) throw new DuplicateEntityException("MainService exists!");
         return repository.save(entity);
     }
 
-    public List<MainService> loadAll(){
-        return repository.findAll();
+    public List<MainService> loadAll() throws EntityNotFoundException {
+        List<MainService> result = repository.findAll();
+        if (result.size()==0)
+            throw new EntityNotFoundException("No Main Service Found!");
+        return result;
     }
 }
