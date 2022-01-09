ebda3dc871d2fd73ead908a21aec1f5e07f1ae8a
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-09 13:56:21 +0330

    add required fields

--- src/main/java/ir/farhanizade/homeservice/dto/out/MainServiceOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/MainServiceOutDto.java
@@ -1,4 +1,14 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
 public class MainServiceOutDto {
+    private Long id;
+    private String name;
+    private List<ServiceOutDto> subServices;
 }
