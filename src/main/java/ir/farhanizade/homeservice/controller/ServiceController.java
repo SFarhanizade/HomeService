e37e6aeee3f3fcaf188c6078fdf55da5498d6881
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-09 13:35:23 +0330

    add @RequestMapping("/services"), MainServiceService and SubServiceService to ServiceController class

--- src/main/java/ir/farhanizade/homeservice/controller/ServiceController.java
+++ src/main/java/ir/farhanizade/homeservice/controller/ServiceController.java
@@ -1,4 +1,17 @@
 package ir.farhanizade.homeservice.controller;
 
+import ir.farhanizade.homeservice.service.MainServiceService;
+import ir.farhanizade.homeservice.service.SubServiceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/services")
+@RequiredArgsConstructor
 public class ServiceController {
+
+    private final MainServiceService mainService;
+    private final SubServiceService subService;
+
 }
