6b9f301ef3bf476d7edf45fccf112d72c858d8b1
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-10 12:51:54 +0330

    add create method to OrderController class

--- src/main/java/ir/farhanizade/homeservice/controller/OrderController.java
+++ src/main/java/ir/farhanizade/homeservice/controller/OrderController.java
@@ -1,4 +1,37 @@
 package ir.farhanizade.homeservice.controller;
 
+import ir.farhanizade.homeservice.controller.api.ResponseResult;
+import ir.farhanizade.homeservice.dto.in.RequestInDto;
+import ir.farhanizade.homeservice.dto.out.EntityOutDto;
+import ir.farhanizade.homeservice.exception.*;
+import ir.farhanizade.homeservice.service.OrderService;
+import ir.farhanizade.homeservice.service.RequestService;
+import ir.farhanizade.homeservice.service.SuggestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
 public class OrderController {
+    private final OrderService orderService;
+    private final RequestService requestService;
+    private final SuggestionService suggestionService;
+
+    @PostMapping
+    public ResponseEntity<ResponseResult<EntityOutDto>> create(@RequestBody RequestInDto request) throws NameNotValidException, NullFieldException, BadEntryException, EmailNotValidException, PasswordNotValidException, EntityNotFoundException {
+        EntityOutDto result = requestService.save(request);
+        ResponseResult<EntityOutDto> response = ResponseResult.<EntityOutDto>builder()
+                .code(1)
+                .message("Order saved successfully!")
+                .data(result)
+                .build();
+        HttpStatus status = HttpStatus.CREATED;
+        return ResponseEntity.status(status).body(response);
+    }
 }
