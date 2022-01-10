93e7afe8d3ea938330d183eda9b93b184b8fcac8
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-10 13:52:16 +0330

    change loadByExpertises method according to the changes of the same method in related repository, in OrderService class

--- src/main/java/ir/farhanizade/homeservice/service/OrderService.java
+++ src/main/java/ir/farhanizade/homeservice/service/OrderService.java
@@ -1,12 +1,15 @@
 package ir.farhanizade.homeservice.service;
 
 import ir.farhanizade.homeservice.entity.order.Order;
+import ir.farhanizade.homeservice.entity.order.OrderStatus;
 import ir.farhanizade.homeservice.entity.service.SubService;
+import ir.farhanizade.homeservice.exception.EntityNotFoundException;
 import ir.farhanizade.homeservice.repository.order.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +20,12 @@ public class OrderService {
         repository.save(order);
     }
 
-    public List<Order> loadByExpertises(List<SubService> expertises){
-        return repository.loadByExpertises(expertises);
+    public List<Order> loadByExpertises(Set<SubService> expertises, OrderStatus status) throws EntityNotFoundException {
+        List<Order> orders = repository.loadByExpertises(expertises, status);
+        if(orders.size() == 0){
+            throw new EntityNotFoundException("No Orders Found!");
+        }
+        return orders;
     }
 
     public List<Order> loadAll(){
