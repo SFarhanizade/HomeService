b29e0018cb96c250cd4f0881d2ae143a9061e6ec
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-10 13:50:43 +0330

    change parameter type of loadByExpertises method from List to Set and add OrderStatus as a new parameter to the method and change the query due to parameters in OrderRepository interface.

--- src/main/java/ir/farhanizade/homeservice/repository/order/OrderRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/OrderRepository.java
@@ -1,14 +1,16 @@
 package ir.farhanizade.homeservice.repository.order;
 
 import ir.farhanizade.homeservice.entity.order.Order;
+import ir.farhanizade.homeservice.entity.order.OrderStatus;
 import ir.farhanizade.homeservice.entity.service.SubService;
 import ir.farhanizade.homeservice.repository.BaseRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Set;
 
 public interface OrderRepository extends BaseRepository<Order> {
 
-    @Query("From Order o where o.service in :expertises")
-    List<Order> loadByExpertises(List<SubService> expertises);
+    @Query("From Order o where o.service in :expertises and o.status=:status")
+    List<Order> loadByExpertises(Set<SubService> expertises, OrderStatus status);
 }
