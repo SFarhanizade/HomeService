73b685ceebf337678d9a3de624c39ed8037b93fe
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-10 21:19:25 +0330

    add findByIdAndCustomerId method to OrderRepository interface

--- src/main/java/ir/farhanizade/homeservice/repository/order/OrderRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/OrderRepository.java
@@ -7,10 +7,15 @@ import ir.farhanizade.homeservice.repository.BaseRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public interface OrderRepository extends BaseRepository<Order> {
 
     @Query("From Order o where o.service in :expertises and o.status=:status")
     List<Order> loadByExpertises(Set<SubService> expertises, OrderStatus status);
+
+    @Query("From Order o where o.id=:orderId and o.request.owner.id=:ownerId")
+    Optional<Order> findByIdAndCustomerId(Long ownerId, Long orderId);
+
 }
