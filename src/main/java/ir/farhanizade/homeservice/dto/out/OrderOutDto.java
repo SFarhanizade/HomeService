495a3f42b545eed94b87315b3a85ce3f599ed69a
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-10 13:41:17 +0330

    add @Builder to OrderOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/OrderOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/OrderOutDto.java
@@ -1,10 +1,12 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
+@Builder
 public class OrderOutDto {
     private Long id;
     private String service;
