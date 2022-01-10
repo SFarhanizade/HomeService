85820f0e6088b6326bf4c3f2bfa3d26027da0be7
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-10 12:19:59 +0330

    add required fields to RequestInDto class

--- src/main/java/ir/farhanizade/homeservice/dto/in/RequestInDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/in/RequestInDto.java
@@ -1,4 +1,26 @@
 package ir.farhanizade.homeservice.dto.in;
 
+import lombok.Data;
+import lombok.NonNull;
+
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Data
 public class RequestInDto {
+
+    @NonNull
+    private Long serviceId;
+    @NonNull
+    private Long ownerId;
+    @NonNull
+    private Long price;
+    @NonNull
+    private Date suggestedDateTime;
+    @NonNull
+    @Size(max = 150)
+    private String details;
+    @NonNull
+    @Size(min = 5, max = 100)
+    private String address;
 }
