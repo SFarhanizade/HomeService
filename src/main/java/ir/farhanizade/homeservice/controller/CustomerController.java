0e410cb2edf726c7e52b352f3581c13f800d005b
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-10 19:29:57 +0330

    change method calling in create method according to the new parameters in CustomerController class

--- src/main/java/ir/farhanizade/homeservice/controller/CustomerController.java
+++ src/main/java/ir/farhanizade/homeservice/controller/CustomerController.java
@@ -3,6 +3,7 @@ package ir.farhanizade.homeservice.controller;
 import ir.farhanizade.homeservice.controller.api.ResponseResult;
 import ir.farhanizade.homeservice.dto.in.UserInDto;
 import ir.farhanizade.homeservice.dto.out.EntityOutDto;
+import ir.farhanizade.homeservice.entity.user.Customer;
 import ir.farhanizade.homeservice.exception.*;
 import ir.farhanizade.homeservice.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +23,6 @@ public class CustomerController {
 
     @PostMapping
     public ResponseEntity<ResponseResult<EntityOutDto>> create(@RequestBody UserInDto user) throws DuplicateEntityException, NameNotValidException, EmailNotValidException, PasswordNotValidException, UserNotValidException, NullFieldException {
-        return userController.create(user);
+        return userController.create(user, Customer.class);
     }
 }
