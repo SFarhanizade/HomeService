92be6b5ea1833c4de9bc8ea8efd3605f8bb021f9
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-10 16:54:05 +0330

    add required fields to ExpertAddSuggestionOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/ExpertAddSuggestionOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/ExpertAddSuggestionOutDto.java
@@ -1,4 +1,12 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class ExpertAddSuggestionOutDto {
+    private Long expertId;
+    private Long orderId;
+    private Long suggestionId;
 }
