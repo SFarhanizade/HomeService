6d19a86d3cea2b13b34fa91e60588c40d77d63ce
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-10 16:50:22 +0330

    add required fields to ExpertAddSuggestionInDto class

--- src/main/java/ir/farhanizade/homeservice/dto/in/ExpertAddSuggestionInDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/in/ExpertAddSuggestionInDto.java
@@ -1,4 +1,14 @@
 package ir.farhanizade.homeservice.dto.in;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class ExpertAddSuggestionInDto {
+    private Long orderId;
+    private Long price;
+    private Date dateTime;
+    private String details;
+    private Double duration;
 }
