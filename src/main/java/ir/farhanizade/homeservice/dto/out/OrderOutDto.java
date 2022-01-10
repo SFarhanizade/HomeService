bd1eafa65b43def75c8fe35ea0829479cfd15f1d
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-10 13:24:58 +0330

    add required fields to OrderOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/OrderOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/OrderOutDto.java
@@ -1,4 +1,14 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class OrderOutDto {
+    private Long id;
+    private String service;
+    private Long price;
+    private Date suggestedDateTime;
+    private Date createdDateTime;
 }
