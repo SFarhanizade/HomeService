216462d55592129b874294eff9d3aae511407d40
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-10 19:33:34 +0330

    add showOrders method to CustomerController class

--- src/main/java/ir/farhanizade/homeservice/controller/CustomerController.java
+++ src/main/java/ir/farhanizade/homeservice/controller/CustomerController.java
@@ -3,16 +3,17 @@ package ir.farhanizade.homeservice.controller;
 import ir.farhanizade.homeservice.controller.api.ResponseResult;
 import ir.farhanizade.homeservice.dto.in.UserInDto;
 import ir.farhanizade.homeservice.dto.out.EntityOutDto;
+import ir.farhanizade.homeservice.dto.out.OrderOutDto;
 import ir.farhanizade.homeservice.entity.user.Customer;
 import ir.farhanizade.homeservice.exception.*;
+import ir.farhanizade.homeservice.service.CustomerService;
 import ir.farhanizade.homeservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customers")
@@ -20,9 +21,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 
     private final UserController userController;
+    private final CustomerService customerService;
 
     @PostMapping
     public ResponseEntity<ResponseResult<EntityOutDto>> create(@RequestBody UserInDto user) throws DuplicateEntityException, NameNotValidException, EmailNotValidException, PasswordNotValidException, UserNotValidException, NullFieldException {
         return userController.create(user, Customer.class);
+    }
+
+    @GetMapping("/{id}/orders")
+    public ResponseEntity<ResponseResult<List<OrderOutDto>>> showOrders(@PathVariable Long id) throws EntityNotFoundException {
+        List<OrderOutDto> result = customerService.getOrders(id);
+        ResponseResult<List<OrderOutDto>> response = ResponseResult.<List<OrderOutDto>>builder()
+                .code(1)
+                .message("List of orders loaded successfully.")
+                .data(result)
+                .build();
+        HttpStatus status = HttpStatus.OK;
+        return ResponseEntity.status(status).body(response);
     }
 }
