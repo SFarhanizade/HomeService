f97d984baa76257048f51c15907eaead732c044d
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-10 21:18:35 +0330

    add findAllByOrderId/OwnerId/CustomerId methods to SuggestionRepository interface

--- src/main/java/ir/farhanizade/homeservice/repository/order/message/SuggestionRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/message/SuggestionRepository.java
@@ -2,6 +2,18 @@ package ir.farhanizade.homeservice.repository.order.message;
 
 import ir.farhanizade.homeservice.entity.order.message.Suggestion;
 import ir.farhanizade.homeservice.repository.BaseRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SuggestionRepository extends BaseRepository<Suggestion> {
+
+    @Query("From Suggestion s where s.order.id=:orderId")
+    List<Suggestion> findAllByOrderId(Long orderId);
+
+    @Query("From Suggestion s where s.owner.id=:ownerId")
+    List<Suggestion> findAllByOwnerId(Long ownerId);
+
+    @Query("From Suggestion s where s.order.request.owner.id=:customerId")
+    List<Suggestion> findAllByCustomerId(Long customerId);
 }
