f8cae8a2c668948c8ff9cc35cc93024161397b5c
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-10 21:18:58 +0330

    add findAllByOrderId/OwnerId/CustomerId methods to SuggestionService class

--- src/main/java/ir/farhanizade/homeservice/service/SuggestionService.java
+++ src/main/java/ir/farhanizade/homeservice/service/SuggestionService.java
@@ -1,6 +1,7 @@
 package ir.farhanizade.homeservice.service;
 
 import ir.farhanizade.homeservice.dto.out.ExpertAddSuggestionOutDto;
+import ir.farhanizade.homeservice.dto.out.SuggestionOutDto;
 import ir.farhanizade.homeservice.entity.order.Order;
 import ir.farhanizade.homeservice.entity.order.message.Suggestion;
 import ir.farhanizade.homeservice.exception.*;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -43,4 +45,52 @@ public class SuggestionService {
     public List<Suggestion> loadAll() {
         return repository.findAll();
     }
+
+    public List<SuggestionOutDto> findAllByOrderId(Long order) throws EntityNotFoundException {
+        List<Suggestion> suggestions = repository.findAllByOrderId(order);
+        if (suggestions.size() == 0)
+            throw new EntityNotFoundException("No Suggestions Found For This Order!");
+        return convert2Dto(suggestions);
+    }
+
+    public SuggestionOutDto findById(Long id) throws EntityNotFoundException {
+        Optional<Suggestion> byId = repository.findById(id);
+        if (byId.isPresent()) {
+            Suggestion suggestion = byId.get();
+            return convert2Dto(suggestion);
+        }
+        throw new EntityNotFoundException("Suggestion Not Found!");
+    }
+
+    public List<SuggestionOutDto> findAllByOwnerId(Long id) throws EntityNotFoundException {
+        List<Suggestion> suggestions = repository.findAllByOwnerId(id);
+        if (suggestions.size() == 0) throw new EntityNotFoundException("No Suggestions Found!");
+        return convert2Dto(suggestions);
+    }
+
+    public List<SuggestionOutDto> findAllByCustomerId(Long id) throws EntityNotFoundException {
+        List<Suggestion> suggestions = repository.findAllByCustomerId(id);
+        if (suggestions.size() == 0) throw new EntityNotFoundException("No Suggestions Found!");
+        return convert2Dto(suggestions);
+    }
+
+    private SuggestionOutDto convert2Dto(Suggestion suggestion) {
+        return SuggestionOutDto.builder()
+                .id(suggestion.getId())
+                .ownerId(suggestion.getOwner().getId())
+                .ownerName(suggestion.getOwner().getFName() + " " + suggestion.getOwner().getLName())
+                .ownerPoints(suggestion.getOwner().getPoints())
+                .createdDateTime(suggestion.getDateTime())
+                .details(suggestion.getDetails())
+                .duration(suggestion.getDuration())
+                .price(suggestion.getPrice())
+                .suggestedDateTime(suggestion.getSuggestedDateTime())
+                .build();
+    }
+
+    private List<SuggestionOutDto> convert2Dto(List<Suggestion> suggestions) {
+        return suggestions.stream()
+                .map(this::convert2Dto).toList();
+    }
+
 }
