6c3d4f9b8a4e25f099526b2012c731d0a24379e2
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-10 20:10:04 +0330

    add required fields to SuggestionOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/SuggestionOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/SuggestionOutDto.java
@@ -1,4 +1,20 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
 public class SuggestionOutDto {
+    private Long id;
+    private BigDecimal price;
+    private Double duration;
+    private String details;
+    private Date suggestedDateTime;
+    private Date createdDateTime;
 }
