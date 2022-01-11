f1886a9cfdff899eed36a3b1cb58ae6acfa9be81
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-12 00:14:52 +0330

    fix the query of findAllByOwnerIdAndStatus method in SuggestionRepository interface

--- src/main/java/ir/farhanizade/homeservice/repository/order/message/SuggestionRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/message/SuggestionRepository.java
@@ -27,6 +27,6 @@ public interface SuggestionRepository extends BaseRepository<Suggestion> {
     @Query("Update Suggestion s set s.suggestionStatus=:rejected where Not s.id=:suggestionId and s.order.id=:orderId")
     void rejectOtherSuggestions(Long suggestionId, Long orderId, SuggestionStatus rejected);
 
-    @Query("From Suggestion s where s.owner.id=:ownerId and s.status in :status")
+    @Query("From Suggestion s where s.owner.id=:ownerId and s.suggestionStatus in :status")
     List<Suggestion> findAllByOwnerIdAndStatus(Long ownerId,SuggestionStatus[] status);
 }
