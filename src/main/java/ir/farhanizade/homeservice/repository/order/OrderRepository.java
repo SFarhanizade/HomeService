a1cc09699956df1fe05d2c868400971b618ae52b
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-11 19:39:21 +0330

    add removeOrderByIdAndOwnerId and acceptSuggestion methods to OrderRepository interface

--- src/main/java/ir/farhanizade/homeservice/repository/order/OrderRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/OrderRepository.java
@@ -4,6 +4,7 @@ import ir.farhanizade.homeservice.entity.order.Order;
 import ir.farhanizade.homeservice.entity.order.OrderStatus;
 import ir.farhanizade.homeservice.entity.service.SubService;
 import ir.farhanizade.homeservice.repository.BaseRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -12,12 +13,24 @@ import java.util.Set;
 
 public interface OrderRepository extends BaseRepository<Order> {
 
-    @Query("From Order o where o.service in :expertises and o.status=:status")
-    List<Order> loadByExpertises(Set<SubService> expertises, OrderStatus status);
+    @Query("From Order o where o.service in :expertises and (o.status=:status1 or o.status=:status2)")
+    List<Order> loadByExpertises(Set<SubService> expertises, OrderStatus status1, OrderStatus status2);
 
     @Query("From Order o where o.id=:orderId and o.request.owner.id=:ownerId")
     Optional<Order> findByIdAndCustomerId(Long ownerId, Long orderId);
 
     @Query("From Order o where o.request.owner.id=:ownerId")
     List<Order> findAllByCustomerId(Long ownerId);
+
+    @Modifying
+    @Query(value = "Delete From my_order " +
+            "Where id =(SELECT r.order_id " +
+                        "FROM request r " +
+                        "WHERE r.owner_id =:owner_id AND r.order_id=:order_id)"
+            , nativeQuery = true)
+    void removeOrderByIdAndOwnerId(Long order_id, Long owner_id);
+
+    @Modifying
+    @Query("Update Order o set o.status=:status where o.id=:id")
+    void acceptSuggestion(Long id, OrderStatus status);
 }
