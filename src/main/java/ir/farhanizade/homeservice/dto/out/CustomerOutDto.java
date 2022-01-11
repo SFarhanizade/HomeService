1341fa797db1374975a07f2e5cb479c278eafc6d
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-11 13:49:35 +0330

    add required fields to CustomerOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/CustomerOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/CustomerOutDto.java
@@ -1,4 +1,9 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import java.math.BigDecimal;
+
 public class CustomerOutDto {
+    private String name;
+    private String email;
+    private BigDecimal credit;
 }
