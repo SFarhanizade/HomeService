992b8a161855be6c1b28f077577d83a7399c81ac
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-11 19:39:57 +0330

    add exists, removeOrderByIdAndOwnerId and acceptSuggestion methods to OrderService class

--- src/main/java/ir/farhanizade/homeservice/service/OrderService.java
+++ src/main/java/ir/farhanizade/homeservice/service/OrderService.java
@@ -14,34 +14,36 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
+import static ir.farhanizade.homeservice.entity.order.OrderStatus.*;
+
 @Service
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository repository;
 
     @Transactional
-    public void save(Order order){
+    public void save(Order order) {
         repository.save(order);
     }
 
     @Transactional(readOnly = true)
-    public List<Order> loadByExpertises(Set<SubService> expertises, OrderStatus status) throws EntityNotFoundException {
-        List<Order> orders = repository.loadByExpertises(expertises, status);
-        if(orders.size() == 0){
+    public List<Order> loadByExpertises(Set<SubService> expertises) throws EntityNotFoundException {
+        List<Order> orders = repository.loadByExpertises(expertises, WAITING_FOR_SUGGESTION, WAITING_FOR_SELECTION);
+        if (orders.size() == 0) {
             throw new EntityNotFoundException("No Orders Found!");
         }
         return orders;
     }
 
     @Transactional(readOnly = true)
-    public List<Order> loadAll(){
+    public List<Order> loadAll() {
         return repository.findAll();
     }
 
     @Transactional(readOnly = true)
     public Order findById(Long id) throws EntityNotFoundException {
         Optional<Order> byId = repository.findById(id);
-        if(byId.isPresent()){
+        if (byId.isPresent()) {
             return byId.get();
         } else throw new EntityNotFoundException("Order Doesn't Exist!");
     }
@@ -49,7 +51,7 @@ public class OrderService {
     @Transactional(readOnly = true)
     public OrderOutDto findByIdAndCustomerId(Long id, Long orderId) throws EntityNotFoundException {
         Optional<Order> byIdAndCustomerId = repository.findByIdAndCustomerId(id, orderId);
-        if(byIdAndCustomerId.isPresent()) {
+        if (byIdAndCustomerId.isPresent()) {
             Order result = byIdAndCustomerId.get();
             return OrderOutDto.builder()
                     .id(result.getId())
@@ -62,7 +64,26 @@ public class OrderService {
         throw new EntityNotFoundException("Order Doesn't Exist!");
     }
 
+    @Transactional(readOnly = true)
     public List<Order> findAllByCustomerId(Long ownerId) {
         return repository.findAllByCustomerId(ownerId);
+    }
+
+    @Transactional
+    public void removeOrderByIdAndOwnerId(Long ownerId, Long orderId) throws EntityNotFoundException {
+        exists(orderId);
+        repository.removeOrderByIdAndOwnerId(orderId, ownerId);
+    }
+
+    @Transactional(readOnly = true)
+    public boolean exists(Long orderId) throws EntityNotFoundException {
+        boolean exists = repository.existsById(orderId);
+        if (exists)
+            return true;
+        throw new EntityNotFoundException("Order Not Found!");
+    }
+
+    public void acceptSuggestion(Long id) {
+        repository.acceptSuggestion(id, WAITING_FOR_EXPERT);
     }
 }
