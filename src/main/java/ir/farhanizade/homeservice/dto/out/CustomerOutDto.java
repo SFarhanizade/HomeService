286dd23b38a4de8e6084c517886dccd35b1cac6a
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-11 13:52:33 +0330

    add @Data and @Builder to CustomerOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/CustomerOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/CustomerOutDto.java
@@ -1,7 +1,12 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.Builder;
+import lombok.Data;
+
 import java.math.BigDecimal;
 
+@Builder
+@Data
 public class CustomerOutDto {
     private String name;
     private String email;
