9f2a5eb7826cad477bbe69c010de65887de53bc6
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-11 20:03:22 +0330

    improve queries of loadByExpertises and removeOrderByIdAndOwnerId methods in OrderRepository interface

--- src/main/java/ir/farhanizade/homeservice/repository/order/OrderRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/OrderRepository.java
@@ -2,6 +2,7 @@ package ir.farhanizade.homeservice.repository.order;
 
 import ir.farhanizade.homeservice.entity.order.Order;
 import ir.farhanizade.homeservice.entity.order.OrderStatus;
+import ir.farhanizade.homeservice.entity.order.message.BaseMessageStatus;
 import ir.farhanizade.homeservice.entity.service.SubService;
 import ir.farhanizade.homeservice.repository.BaseRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -13,8 +14,13 @@ import java.util.Set;
 
 public interface OrderRepository extends BaseRepository<Order> {
 
-    @Query("From Order o where o.service in :expertises and (o.status=:status1 or o.status=:status2)")
-    List<Order> loadByExpertises(Set<SubService> expertises, OrderStatus status1, OrderStatus status2);
+    @Query("From Order o " +
+            "where o.service in :expertises " +
+            "and (o.status=:forSuggestion or o.status=:forSelection)" +
+            "and not (o.request.status=:cancelled or o.request.status=:busy)")
+    List<Order> loadByExpertises(Set<SubService> expertises,
+                                 OrderStatus forSuggestion, OrderStatus forSelection,
+                                 BaseMessageStatus cancelled, BaseMessageStatus busy);
 
     @Query("From Order o where o.id=:orderId and o.request.owner.id=:ownerId")
     Optional<Order> findByIdAndCustomerId(Long ownerId, Long orderId);
@@ -23,12 +29,8 @@ public interface OrderRepository extends BaseRepository<Order> {
     List<Order> findAllByCustomerId(Long ownerId);
 
     @Modifying
-    @Query(value = "Delete From my_order " +
-            "Where id =(SELECT r.order_id " +
-                        "FROM request r " +
-                        "WHERE r.owner_id =:owner_id AND r.order_id=:order_id)"
-            , nativeQuery = true)
-    void removeOrderByIdAndOwnerId(Long order_id, Long owner_id);
+    @Query("Update Request r set r.status=:cancelled where r.owner.id=:owner_id and r.order.id=:order_id")
+    void removeOrderByIdAndOwnerId(Long order_id, Long owner_id, BaseMessageStatus cancelled);
 
     @Modifying
     @Query("Update Order o set o.status=:status where o.id=:id")
