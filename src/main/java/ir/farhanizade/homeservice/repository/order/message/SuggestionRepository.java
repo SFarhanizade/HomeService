d9453adb23ed8f9b5ec7ec917826a4395d2a28cb
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-11 19:36:23 +0330

    add acceptSuggestion and rejectSuggestions methods to SuggestionRepository interface

--- src/main/java/ir/farhanizade/homeservice/repository/order/message/SuggestionRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/message/SuggestionRepository.java
@@ -1,7 +1,9 @@
 package ir.farhanizade.homeservice.repository.order.message;
 
 import ir.farhanizade.homeservice.entity.order.message.Suggestion;
+import ir.farhanizade.homeservice.entity.order.message.SuggestionStatus;
 import ir.farhanizade.homeservice.repository.BaseRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
@@ -16,4 +18,12 @@ public interface SuggestionRepository extends BaseRepository<Suggestion> {
 
     @Query("From Suggestion s where s.order.request.owner.id=:customerId")
     List<Suggestion> findAllByCustomerId(Long customerId);
+
+    @Modifying
+    @Query("Update Suggestion s set s.suggestionStatus=:accepted where s.id=:suggestionId")
+    void acceptSuggestion(Long suggestionId, SuggestionStatus accepted);
+
+    @Modifying
+    @Query("Update Suggestion s set s.suggestionStatus=:rejected where Not s.id=:suggestionId and s.order.id=:orderId")
+    void rejectOtherSuggestions(Long suggestionId, Long orderId, SuggestionStatus rejected);
 }
