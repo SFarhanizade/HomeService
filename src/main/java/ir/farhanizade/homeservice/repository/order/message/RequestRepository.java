dd0632953e85f4cf6bb28ae9bd2bda55e3f14cc1
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-12 14:18:55 +0330

    add cancel method to RequestRepository interface

--- src/main/java/ir/farhanizade/homeservice/repository/order/message/RequestRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/message/RequestRepository.java
@@ -3,9 +3,15 @@ package ir.farhanizade.homeservice.repository.order.message;
 import ir.farhanizade.homeservice.entity.order.message.BaseMessageStatus;
 import ir.farhanizade.homeservice.entity.order.message.Request;
 import ir.farhanizade.homeservice.repository.BaseRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface RequestRepository extends BaseRepository<Request> {
     List<Request> findByStatus(BaseMessageStatus waiting);
+
+    @Modifying
+    @Query("Update Request r set r.status=:cancelled where r.order.id=:orderId")
+    void cancel(Long orderId, BaseMessageStatus cancelled);
 }
