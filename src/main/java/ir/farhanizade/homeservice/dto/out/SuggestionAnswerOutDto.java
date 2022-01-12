c3efcc6748cb8d1448f776f3d2d3a6f29c9f39af
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-12 16:02:59 +0330

    add required fields to SuggestionAnswerOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/SuggestionAnswerOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/SuggestionAnswerOutDto.java
@@ -1,4 +1,13 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import ir.farhanizade.homeservice.entity.order.message.BaseMessageStatus;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
 public class SuggestionAnswerOutDto {
+    private Long suggestion;
+    private Long orderId;
+    private BaseMessageStatus answer;
 }
