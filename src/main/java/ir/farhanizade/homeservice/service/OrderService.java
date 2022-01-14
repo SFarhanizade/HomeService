3bf5f650c2d06f998a2f6e465552dd70da496230
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-14 15:49:35 +0330

    refactor removeOrderByIdAndOwnerId and acceptSuggestion methods, remove changeStatus and exists methods, from OrderService class

--- src/main/java/ir/farhanizade/homeservice/service/OrderService.java
+++ src/main/java/ir/farhanizade/homeservice/service/OrderService.java
@@ -3,9 +3,8 @@ package ir.farhanizade.homeservice.service;
 import ir.farhanizade.homeservice.dto.out.EntityOutDto;
 import ir.farhanizade.homeservice.dto.out.OrderOutDto;
 import ir.farhanizade.homeservice.entity.order.Order;
-import ir.farhanizade.homeservice.entity.order.OrderStatus;
+import ir.farhanizade.homeservice.entity.order.message.Request;
 import ir.farhanizade.homeservice.entity.order.message.Suggestion;
-import ir.farhanizade.homeservice.entity.order.message.SuggestionStatus;
 import ir.farhanizade.homeservice.entity.service.SubService;
 import ir.farhanizade.homeservice.exception.*;
 import ir.farhanizade.homeservice.repository.order.OrderRepository;
@@ -13,21 +12,20 @@ import ir.farhanizade.homeservice.service.util.Validation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-
 import static ir.farhanizade.homeservice.entity.order.OrderStatus.*;
 import static ir.farhanizade.homeservice.entity.order.message.BaseMessageStatus.BUSY;
 import static ir.farhanizade.homeservice.entity.order.message.BaseMessageStatus.CANCELLED;
+import static ir.farhanizade.homeservice.entity.order.message.SuggestionStatus.ACCEPTED;
+import static ir.farhanizade.homeservice.entity.order.message.SuggestionStatus.REJECTED;
 
 @Service
 @RequiredArgsConstructor
 public class OrderService {
     private final OrderRepository repository;
     private final SuggestionService suggestionService;
-    private final RequestService requestService;
 
     @Transactional
     public void save(Order order) {
@@ -36,7 +34,7 @@ public class OrderService {
 
     @Transactional(readOnly = true)
     public List<Order> loadByExpertises(Set<SubService> expertises) throws EntityNotFoundException {
-        List<Order> orders = repository.loadByExpertises(expertises, WAITING_FOR_SUGGESTION, WAITING_FOR_SELECTION,CANCELLED,BUSY);
+        List<Order> orders = repository.loadByExpertises(expertises, WAITING_FOR_SUGGESTION, WAITING_FOR_SELECTION, CANCELLED, BUSY);
         if (orders.size() == 0) {
             throw new EntityNotFoundException("No Orders Found!");
         }
@@ -79,33 +77,32 @@ public class OrderService {
     }
 
     @Transactional
-    public void removeOrderByIdAndOwnerId(Long ownerId, Long orderId) throws EntityNotFoundException {
-        findById(orderId);
-        repository.removeOrderByIdAndOwnerId(orderId, ownerId, CANCELLED);
-        requestService.changeStatus(orderId, CANCELLED);
-        suggestionService.cancel(orderId);
-    }
-
-    @Transactional(readOnly = true)
-    public boolean exists(Long orderId) throws EntityNotFoundException {
-        boolean exists = repository.existsById(orderId);
-        if (exists)
-            return true;
-        throw new EntityNotFoundException("Order Not Found!");
+    public void removeOrderByIdAndOwnerId(Long orderId, Long ownerId) throws EntityNotFoundException {
+        Order order = findById(orderId);
+        Request request = order.getRequest();
+        if (!request.getOwner().getId().equals(ownerId)) throw new EntityNotFoundException("This order is not yours!");
+        request.setStatus(CANCELLED);
+        Set<Suggestion> suggestions = order.getSuggestions();
+        suggestions.stream()
+                .forEach(s -> {
+                    s.setSuggestionStatus(REJECTED);
+                    s.setStatus(CANCELLED);
+                });
+        repository.save(order);
     }
 
     @Transactional
-    public EntityOutDto acceptSuggestion(Long id) throws BusyOrderException, NameNotValidException, EmailNotValidException, PasswordNotValidException, NullFieldException, BadEntryException, EntityNotFoundException {
-        Suggestion suggestion = suggestionService.loadById(id);
+    public EntityOutDto acceptSuggestion(Long id) throws BusyOrderException, NameNotValidException, EmailNotValidException, PasswordNotValidException, NullFieldException, BadEntryException, EntityNotFoundException, DuplicateEntityException {
+        Suggestion suggestion = suggestionService.findById(id);
         Validation.isValid(suggestion);
         Order order = suggestion.getOrder();
-        repository.changeStatus(id, WAITING_FOR_EXPERT);
-        suggestionService.acceptSuggestion(id,order.getId());
+        order.setStatus(WAITING_FOR_EXPERT);
+        suggestion.setSuggestionStatus(ACCEPTED);
+        Set<Suggestion> suggestions = order.getSuggestions();
+        suggestions.stream()
+                .filter(s -> !s.equals(suggestion))
+                .forEach(s -> s.setSuggestionStatus(REJECTED));
+        suggestionService.save(suggestion);
         return new EntityOutDto(id);
-    }
-
-    public void changeStatus(Long id, OrderStatus status) throws EntityNotFoundException {
-        exists(id);
-        repository.changeStatus(id,status);
     }
 }
