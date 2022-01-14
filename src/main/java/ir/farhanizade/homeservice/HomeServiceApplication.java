6138c94b1d4af8235a405ba535eaa14c9381ea39
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-14 12:58:08 +0330

    add @EnableJpaAuditing to HomeServiceApplication class

--- src/main/java/ir/farhanizade/homeservice/HomeServiceApplication.java
+++ src/main/java/ir/farhanizade/homeservice/HomeServiceApplication.java
@@ -2,8 +2,10 @@ package ir.farhanizade.homeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HomeServiceApplication {
 
     public static void main(String[] args) {
