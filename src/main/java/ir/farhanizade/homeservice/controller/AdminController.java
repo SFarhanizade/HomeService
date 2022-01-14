f709c805af2e4fc1d2c95756c82d2855734d0765
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-14 06:44:29 +0330

    add create method to AdminController class

--- src/main/java/ir/farhanizade/homeservice/controller/AdminController.java
+++ src/main/java/ir/farhanizade/homeservice/controller/AdminController.java
@@ -1,4 +1,29 @@
 package ir.farhanizade.homeservice.controller;
 
+import ir.farhanizade.homeservice.controller.api.ResponseResult;
+import ir.farhanizade.homeservice.dto.in.UserInDto;
+import ir.farhanizade.homeservice.dto.out.EntityOutDto;
+import ir.farhanizade.homeservice.entity.user.Admin;
+import ir.farhanizade.homeservice.entity.user.Customer;
+import ir.farhanizade.homeservice.exception.*;
+import ir.farhanizade.homeservice.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admins")
+@RequiredArgsConstructor
 public class AdminController {
+    private final UserController userController;
+    private final AdminService adminService;
+
+    @PostMapping
+    public ResponseEntity<ResponseResult<EntityOutDto>> create(@RequestBody UserInDto user) throws DuplicateEntityException, NameNotValidException, EmailNotValidException, PasswordNotValidException, UserNotValidException, NullFieldException {
+        return userController.create(user, Admin.class);
+    }
+
 }
