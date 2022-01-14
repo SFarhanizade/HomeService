322cd211828ce1c89c1639bfd8feebd789ee01fe
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-14 15:35:24 +0330

    remove all methods from UserRepository interface

--- src/main/java/ir/farhanizade/homeservice/repository/user/UserRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/user/UserRepository.java
@@ -2,15 +2,7 @@ package ir.farhanizade.homeservice.repository.user;
 
 import ir.farhanizade.homeservice.entity.user.User;
 import ir.farhanizade.homeservice.repository.BaseRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends BaseRepository<User>, CustomUserRepository {
 
-    @Query("From User u where u.id=:id and u.password=:currentPassword")
-    User isCorrectByPassword(Long id, String currentPassword);
-
-    @Modifying
-    @Query("Update User u set u.password=:newPassword where u.id=:id")
-    int updatePassword(Long id, String newPassword);
 }
