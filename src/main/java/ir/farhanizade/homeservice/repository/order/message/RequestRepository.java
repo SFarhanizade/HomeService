ee6906cdec773bbea120862ed7e25ff480d9a381
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-14 15:36:30 +0330

    remove changeStatus method from RequestRepository interface

--- src/main/java/ir/farhanizade/homeservice/repository/order/message/RequestRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/message/RequestRepository.java
@@ -3,15 +3,14 @@ package ir.farhanizade.homeservice.repository.order.message;
 import ir.farhanizade.homeservice.entity.order.message.BaseMessageStatus;
 import ir.farhanizade.homeservice.entity.order.message.Request;
 import ir.farhanizade.homeservice.repository.BaseRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RequestRepository extends BaseRepository<Request> {
     List<Request> findByStatus(BaseMessageStatus waiting);
 
-    @Modifying
-    @Query("Update Request r set r.status=:cancelled where r.order.id=:orderId")
-    void changeStatus(Long orderId, BaseMessageStatus cancelled);
+    @Query("From Request r where r.order.id=:orderId")
+    Optional<Request> findByOrderId(Long orderId);
 }
