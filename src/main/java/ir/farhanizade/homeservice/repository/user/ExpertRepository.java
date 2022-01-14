9c8161dc363c8d456677ffaf939d61949420cdbd
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-14 06:55:14 +0330

    add acceptExpert method to ExpertRepository interface

--- src/main/java/ir/farhanizade/homeservice/repository/user/ExpertRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/user/ExpertRepository.java
@@ -5,6 +5,7 @@ import ir.farhanizade.homeservice.entity.user.Customer;
 import ir.farhanizade.homeservice.entity.user.Expert;
 import ir.farhanizade.homeservice.entity.user.UserStatus;
 import ir.farhanizade.homeservice.repository.BaseRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
@@ -19,4 +20,8 @@ public interface ExpertRepository extends BaseRepository<Expert>, CustomExpertRe
     //@Query(value = "select u.* From user_expertises e, user u where e.expertises_id = :service and e.expert_id = u.id", nativeQuery = true)
     @Query("From Expert e inner join e.expertises s where e.id=:service")
     List<Expert> findByExpertise(Long service);
+
+    @Modifying
+    @Query("Update Expert e set e.status=:accepted where e.id=:id")
+    void acceptExpert(Long id, UserStatus accepted);
 }
