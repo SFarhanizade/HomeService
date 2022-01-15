1a932f7b842759bc3045227a72153c8e9e4a6e38
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-15 21:35:29 +0330

    change return type of loadByExpertises method from Page<Order> to CustomPage<Order> in CustomOrderRepository interface

--- src/main/java/ir/farhanizade/homeservice/repository/order/CustomOrderRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/CustomOrderRepository.java
@@ -1,14 +1,17 @@
 package ir.farhanizade.homeservice.repository.order;
 
 import ir.farhanizade.homeservice.dto.in.UserSearchInDto;
+import ir.farhanizade.homeservice.entity.CustomPage;
 import ir.farhanizade.homeservice.entity.order.Order;
+import ir.farhanizade.homeservice.entity.service.SubService;
 import ir.farhanizade.homeservice.entity.user.Customer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
+import java.util.Set;
 
 public interface CustomOrderRepository {
-    Page<Order> loadByExpertises(Specification<Order> spec, Pageable pageable);
+    CustomPage<Order> findByExpertises(Set<SubService> expertises, Pageable pageable);
 }
