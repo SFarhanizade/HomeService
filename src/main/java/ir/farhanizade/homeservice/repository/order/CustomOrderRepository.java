708634682f44cb13289b06a9ab8083d9fca4587d
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-15 18:17:47 +0330

    create CustomOrderRepository interface in repository.order package

---
+++ src/main/java/ir/farhanizade/homeservice/repository/order/CustomOrderRepository.java
@@ -0,0 +1,10 @@
+package ir.farhanizade.homeservice.repository.order;
+
+import ir.farhanizade.homeservice.dto.in.UserSearchInDto;
+import ir.farhanizade.homeservice.entity.user.Customer;
+
+import java.util.List;
+
+public interface CustomOrderRepository {
+
+}
