4144b33de447f53c035cf5fb89552a595e39c518
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-15 21:02:25 +0330

    add required fields to CustomPage class

--- src/main/java/ir/farhanizade/homeservice/entity/CustomPage.java
+++ src/main/java/ir/farhanizade/homeservice/entity/CustomPage.java
@@ -1,4 +1,15 @@
 package ir.farhanizade.homeservice.entity;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
 public class CustomPage<T> {
+    private Integer pageNumber;
+    private List<T> data;
+    private Integer lastPage;
+    private Integer pageSize;
 }
