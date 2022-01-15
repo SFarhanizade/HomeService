e286cb94c291106d986f4fa9126c39ef28dce803
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-15 18:20:19 +0330

    add loadByExpertises method to CustomOrderRepository interface

--- src/main/java/ir/farhanizade/homeservice/repository/order/CustomOrderRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/CustomOrderRepository.java
@@ -1,10 +1,14 @@
 package ir.farhanizade.homeservice.repository.order;
 
 import ir.farhanizade.homeservice.dto.in.UserSearchInDto;
+import ir.farhanizade.homeservice.entity.order.Order;
 import ir.farhanizade.homeservice.entity.user.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
 
 public interface CustomOrderRepository {
-
+    Page<Order> loadByExpertises(Specification<Order> spec, Pageable pageable);
 }
