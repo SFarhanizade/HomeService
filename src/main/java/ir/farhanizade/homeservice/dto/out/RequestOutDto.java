f185cb678b20cf29601b36e8a2b3c49124825f4f
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-16 15:26:26 +0330

    add required fields to RequestOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/RequestOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/RequestOutDto.java
@@ -1,4 +1,20 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import ir.farhanizade.homeservice.entity.order.OrderStatus;
+import ir.farhanizade.homeservice.entity.order.message.BaseMessageStatus;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Builder
 public class RequestOutDto {
+    private String service;
+    private Date suggestedDateTime;
+    private Date createdDateTime;
+    private BigDecimal price;
+    private BaseMessageStatus status;
+    private OrderStatus orderStatus;
 }
