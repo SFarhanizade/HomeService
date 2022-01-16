ad08f05b88605b8e07fed9fae08021b45e5a5cd9
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-16 18:35:27 +0330

    add required fields to CommentInDto class

--- src/main/java/ir/farhanizade/homeservice/dto/in/CommentInDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/in/CommentInDto.java
@@ -1,4 +1,14 @@
 package ir.farhanizade.homeservice.dto.in;
 
+import lombok.Data;
+import lombok.NonNull;
+
+import javax.validation.constraints.Size;
+
+@Data
 public class CommentInDto {
+    @NonNull
+    @Size(min = 1, max = 5)
+    private Integer points;
+    private String description;
 }
