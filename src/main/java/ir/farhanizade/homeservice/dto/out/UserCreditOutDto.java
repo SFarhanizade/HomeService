f23c18a3f1e1a116d8f66e5505e9e69b2cc2ad0a
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-16 23:57:16 +0330

    add required fields to UserCreditOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/UserCreditOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/UserCreditOutDto.java
@@ -1,4 +1,13 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@Data
 public class UserCreditOutDto {
+    private Long id;
+    private BigDecimal credit;
 }
