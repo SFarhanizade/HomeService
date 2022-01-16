b7993ea9d940c0dc1d0462b7f818dc8820b0f0d8
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-16 19:01:07 +0330

    add findByCustomerId method to TransactionRepository interface

--- src/main/java/ir/farhanizade/homeservice/repository/TransactionRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/TransactionRepository.java
@@ -1,6 +1,11 @@
 package ir.farhanizade.homeservice.repository;
 
 import ir.farhanizade.homeservice.entity.Transaction;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TransactionRepository extends BaseRepository<Transaction>{
+    @Query("From Transaction t where t.payer.id=:id")
+    Page<Transaction> findByCustomerId(Long id, Pageable pageable);
 }
