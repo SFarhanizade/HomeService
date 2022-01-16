7379b7f82e3d09d8786ae76bc0160cf66299030a
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-16 18:51:41 +0330

    add required fields to TransactionOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/TransactionOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/TransactionOutDto.java
@@ -1,4 +1,18 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Builder
+@Data
 public class TransactionOutDto {
+    private Long expertId;
+    private String expertName;
+    private Long customerId;
+    private String customerName;
+    private BigDecimal amount;
+    private Date dateTime;
 }
