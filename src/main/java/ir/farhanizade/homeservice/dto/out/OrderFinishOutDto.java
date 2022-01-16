5a77d7673d22b75979ae15487cb95cda56108565
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-16 21:09:57 +0330

    add required fields to OrderFinishOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/OrderFinishOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/OrderFinishOutDto.java
@@ -1,4 +1,19 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import ir.farhanizade.homeservice.entity.order.OrderStatus;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Builder
 public class OrderFinishOutDto {
+    private Long id;
+    private String service;
+    private BigDecimal price;
+    private Date startDateTime;
+    private Date finishDateTime;
+    private OrderStatus status;
 }
