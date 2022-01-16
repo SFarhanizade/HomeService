4b62fc751a4734937d6b6ec618b01c483c66917a
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-16 19:49:17 +0330

    add required fields to CommentOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/CommentOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/CommentOutDto.java
@@ -1,4 +1,18 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.Date;
+
+@Builder
+@Data
 public class CommentOutDto {
+    private Long id;
+    private Long customerId;
+    private String customerName;
+    private Long expertId;
+    private String expertName;
+    private Long orderId;
+    private Date dateTime;
 }
