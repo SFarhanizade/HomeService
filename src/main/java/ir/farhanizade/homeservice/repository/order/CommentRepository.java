192892e68f5e00cafc2aad21fed34f3361749382
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-16 20:04:02 +0330

    add findAllByCustomerId method to CommentRepository interface

--- src/main/java/ir/farhanizade/homeservice/repository/order/CommentRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/CommentRepository.java
@@ -1,7 +1,14 @@
 package ir.farhanizade.homeservice.repository.order;
 
+import ir.farhanizade.homeservice.dto.out.CommentOutDto;
 import ir.farhanizade.homeservice.entity.order.Comment;
 import ir.farhanizade.homeservice.repository.BaseRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CommentRepository extends BaseRepository<Comment> {
+
+    @Query("From Comment c where c.customer.id=:id")
+    Page<Comment> findAllByCustomerId(Long id, Pageable pageable);
 }
