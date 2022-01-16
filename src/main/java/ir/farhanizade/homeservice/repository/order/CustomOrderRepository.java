02d8e9967695c4ef5f9974744c44f8c4c6fc5f48
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-16 16:17:57 +0330

    remove methods from CustomOrderRepository interface/class

--- src/main/java/ir/farhanizade/homeservice/repository/order/CustomOrderRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/CustomOrderRepository.java
@@ -1,17 +1,4 @@
 package ir.farhanizade.homeservice.repository.order;
 
-import ir.farhanizade.homeservice.dto.in.UserSearchInDto;
-import ir.farhanizade.homeservice.entity.CustomPage;
-import ir.farhanizade.homeservice.entity.order.Order;
-import ir.farhanizade.homeservice.entity.service.SubService;
-import ir.farhanizade.homeservice.entity.user.Customer;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
-
-import java.util.List;
-import java.util.Set;
-
 public interface CustomOrderRepository {
-    CustomPage<Order> findByExpertises(Set<SubService> expertises, Pageable pageable);
 }
--- src/main/java/ir/farhanizade/homeservice/repository/order/CustomOrderRepositoryImpl.java
+++ src/main/java/ir/farhanizade/homeservice/repository/order/CustomOrderRepositoryImpl.java
@@ -22,46 +22,4 @@ public class CustomOrderRepositoryImpl implements CustomOrderRepository {
 
     @Autowired
     EntityManager em;
-
-    @Override
-    public CustomPage<Order> findByExpertises(Set<SubService> expertises, Pageable pageable) {
-        int pageNumber = pageable.getPageNumber()+1;
-        int pageSize = pageable.getPageSize();
-        List<Long> subServices = expertises.stream().map(s -> s.getId()).toList();
-
-        CriteriaBuilder cb = em.getCriteriaBuilder();
-        CriteriaQuery<Order> cq = cb.createQuery(Order.class);
-        Root<Order> from = cq.from(Order.class);
-        Join join = from.join("service");
-
-        Expression<Long> ids = join.get("id");
-        Predicate expertise = ids.in(subServices);
-        Predicate status1 = cb.equal(from.get("status"), WAITING_FOR_SUGGESTION);
-        Predicate status2 = cb.equal(from.get("status"), WAITING_FOR_SELECTION);
-        Predicate status = cb.or(status1, status2);
-        cq.where(expertise, status);
-
-        TypedQuery<Order> query = em.createQuery(cq);
-
-
-        CriteriaQuery<Long> countQuery = cb.createQuery(Long.class);
-        countQuery.from(Order.class).join("service");
-        countQuery.select(cb.count(from));
-        countQuery.where(expertise,status);
-        Long count = em.createQuery(countQuery).getSingleResult();
-        int totalPage = (count.intValue() / 10) + 1;
-
-        List<Order> resultList = new ArrayList<>();
-        if (pageNumber <=totalPage) {
-            query.setFirstResult(pageNumber - 1);
-            query.setMaxResults(pageSize);
-            resultList = query.getResultList();
-        }
-        return CustomPage.<Order>builder()
-                .pageNumber(pageNumber)
-                .lastPage(totalPage)
-                .pageSize(pageSize)
-                .data(resultList)
-                .build();
-    }
 }
