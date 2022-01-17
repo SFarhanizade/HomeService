139225fb356d3173ff534186b12dfd8976b53136
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-17 21:57:48 +0330

    add @NoArgsConstructor and @AllArgsConstructor to TransactionOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/TransactionOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/TransactionOutDto.java
@@ -1,13 +1,17 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransactionOutDto {
     private Long id;
     private Long expertId;
