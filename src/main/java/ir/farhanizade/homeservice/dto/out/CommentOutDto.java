efeeb0bc6aba78facf6d2245f75c9471bcf19dd8
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-17 21:58:19 +0330

    add @NoArgsConstructor and @AllArgsConstructor to CommentOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/CommentOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/CommentOutDto.java
@@ -1,12 +1,16 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CommentOutDto {
     private Long id;
     private Long customerId;
