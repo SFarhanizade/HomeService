01a23e7d0da1c90effcf6c3df9e98ca1335931e3
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-17 15:12:10 +0330

    create CustomerControllerTest class in test package

---
+++ src/test/java/ir/farhanizade/homeservice/controller/CustomerControllerTest.java
@@ -0,0 +1,76 @@
+package ir.farhanizade.homeservice.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void showOrders() {
+    }
+
+    @Test
+    void request() {
+    }
+
+    @Test
+    void showOrder() {
+    }
+
+    @Test
+    void removeOrder() {
+    }
+
+    @Test
+    void showSuggestionsByOrder() {
+    }
+
+    @Test
+    void showAllSuggestions() {
+    }
+
+    @Test
+    void showSuggestion() {
+    }
+
+    @Test
+    void acceptSuggestion() {
+    }
+
+    @Test
+    void pay() {
+    }
+
+    @Test
+    void showTransactions() {
+    }
+
+    @Test
+    void showTransaction() {
+    }
+
+    @Test
+    void comment() {
+    }
+
+    @Test
+    void showComments() {
+    }
+
+    @Test
+    void showComment() {
+    }
+
+    @Test
+    void showCredit() {
+    }
+
+    @Test
+    void addCredit() {
+    }
+}
