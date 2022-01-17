70e7228b84b05ba42356b95793b5e767b5c9651f
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-17 22:20:51 +0330

    add simple functionality tests for all methods of AdminController class, in AdminControllerTest class

--- src/test/java/ir/farhanizade/homeservice/controller/AdminControllerTest.java
+++ src/test/java/ir/farhanizade/homeservice/controller/AdminControllerTest.java
@@ -1,16 +1,75 @@
 package ir.farhanizade.homeservice.controller;
 
+import ir.farhanizade.homeservice.controller.api.ResponseResult;
+import ir.farhanizade.homeservice.dto.in.UserInDto;
+import ir.farhanizade.homeservice.dto.out.EntityOutDto;
+import ir.farhanizade.homeservice.entity.user.Admin;
+import ir.farhanizade.homeservice.service.AdminService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class AdminControllerTest {
+@RunWith(SpringRunner.class)
+@WebMvcTest(AdminController.class)
+class AdminControllerTest extends AbstractRestControllerTest {
+
+    @MockBean
+    private UserController userController;
+
+    @MockBean
+    private AdminService adminService;
 
     @Test
-    void create() {
+    void test_create_isOk() throws Exception {
+        UserInDto admin = UserInDto.builder()
+                .firstname("admin")
+                .lastname("adminPour")
+                .email("admin@admin.ir")
+                .password("abcd1234")
+                .build();
+
+        ResponseResult<EntityOutDto> response = ResponseResult.<EntityOutDto>builder()
+                .code(1)
+                .data(new EntityOutDto(1L))
+                .message("User saved successfully!")
+                .build();
+
+        ResponseEntity<ResponseResult<EntityOutDto>> returnValue = ResponseEntity.status(HttpStatus.CREATED).body(response);
+
+        Mockito.when(userController.create(admin, Admin.class))
+                .thenReturn(returnValue);
+
+        mvc.perform(post("/admins")
+                        .content(toJson(admin))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.data").value(new EntityOutDto(1L)));
+
     }
 
     @Test
-    void acceptExpert() {
+    void test_acceptExpert_isOk() throws Exception {
+        EntityOutDto result = new EntityOutDto(1L);
+
+        Mockito.when(adminService.acceptExpert(1L, 1L))
+                .thenReturn(result);
+
+        mvc.perform(get("/admins/1/experts/1/accept"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.data.id").value(1L));
     }
 }
