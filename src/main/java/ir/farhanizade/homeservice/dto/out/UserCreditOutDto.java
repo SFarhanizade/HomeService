1f61fc9a0324698885d37ed967ec9775ee98e169
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-17 21:57:26 +0330

    add @NoArgsConstructor to UserCreditOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/UserCreditOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/UserCreditOutDto.java
@@ -2,11 +2,13 @@ package ir.farhanizade.homeservice.dto.out;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @AllArgsConstructor
 @Data
+@NoArgsConstructor
 public class UserCreditOutDto {
     private Long id;
     private BigDecimal credit;
