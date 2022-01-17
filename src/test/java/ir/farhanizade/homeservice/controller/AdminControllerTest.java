d128f5011c70e94530a102af731179544c92a3fa
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-17 22:08:27 +0330

    create AdminControllerTest in test package

---
+++ src/test/java/ir/farhanizade/homeservice/controller/AdminControllerTest.java
@@ -0,0 +1,16 @@
+package ir.farhanizade.homeservice.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void acceptExpert() {
+    }
+}
