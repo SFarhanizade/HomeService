75b178f2a74eeab4d182f5d59a4229aa4c3f0a17
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-17 22:21:29 +0330

    add @AllArgsConstructor and @NoArgsConstructor to ExpertInDto class

--- src/main/java/ir/farhanizade/homeservice/dto/in/ExpertInDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/in/ExpertInDto.java
@@ -1,8 +1,12 @@
 package ir.farhanizade.homeservice.dto.in;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ExpertInDto {
     private Long id;
 }
