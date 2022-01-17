bee10f8d2223464a3bc273b3fbfe733e5bdb0853
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-17 22:22:39 +0330

    add simple functionality tests for all methods of OrderController class, in OrderControllerTest class

--- src/test/java/ir/farhanizade/homeservice/controller/OrderControllerTest.java
+++ src/test/java/ir/farhanizade/homeservice/controller/OrderControllerTest.java
@@ -1,12 +1,47 @@
 package ir.farhanizade.homeservice.controller;
 
+import ir.farhanizade.homeservice.dto.in.ExpertInDto;
+import ir.farhanizade.homeservice.dto.out.OrderOutDto;
+import ir.farhanizade.homeservice.entity.CustomPage;
+import ir.farhanizade.homeservice.service.ExpertService;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class OrderControllerTest {
+@RunWith(SpringRunner.class)
+@WebMvcTest(OrderController.class)
+class OrderControllerTest extends AbstractRestControllerTest {
+
+    @MockBean
+    private ExpertService expertService;
 
     @Test
-    void showList() {
+    void showList() throws Exception {
+        ExpertInDto request = new ExpertInDto();
+
+        CustomPage<OrderOutDto> result = CustomPage.<OrderOutDto>builder()
+                .data(List.of(new OrderOutDto(), new OrderOutDto(), new OrderOutDto())).build();
+
+        Mockito.when(expertService.loadAvailableOrders(request, Pageable.ofSize(20)))
+                .thenReturn(result);
+
+        mvc.perform(post("/orders")
+                        .content(toJson(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.data", hasSize(3)));
     }
 }
