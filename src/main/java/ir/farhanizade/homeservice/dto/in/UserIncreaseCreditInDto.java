36fad8e5c2afba1d04ab1d3e99f18ba1418e7748
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-17 18:16:24 +0330

    add @NoArgsConstructor to UserIncreaseCreditInDto class

--- src/main/java/ir/farhanizade/homeservice/dto/in/UserIncreaseCreditInDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/in/UserIncreaseCreditInDto.java
@@ -2,9 +2,11 @@ package ir.farhanizade.homeservice.dto.in;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserIncreaseCreditInDto {
     private Long amount;
 }
