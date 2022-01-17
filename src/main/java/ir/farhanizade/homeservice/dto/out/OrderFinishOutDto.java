614628a8c1723b6632100c99f049c4f5494fcd25
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-17 21:58:06 +0330

    add @NoArgsConstructor and @AllArgsConstructor to OrderFinishOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/OrderFinishOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/OrderFinishOutDto.java
@@ -1,14 +1,18 @@
 package ir.farhanizade.homeservice.dto.out;
 
 import ir.farhanizade.homeservice.entity.order.OrderStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderFinishOutDto {
     private Long id;
     private String service;
