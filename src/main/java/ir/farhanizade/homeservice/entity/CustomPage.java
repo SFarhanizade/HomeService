b9e02e4e340e3b45179144cc43275cb500d95acb
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-17 15:05:40 +0330

    add @NoArgsConstructor and @AllArgsConstructor to CustomPage class

--- src/main/java/ir/farhanizade/homeservice/entity/CustomPage.java
+++ src/main/java/ir/farhanizade/homeservice/entity/CustomPage.java
@@ -1,14 +1,18 @@
 package ir.farhanizade.homeservice.entity;
 
 import ir.farhanizade.homeservice.dto.out.UserOutDto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomPage<T> {
     private Integer pageNumber;
     private List<T> data;
