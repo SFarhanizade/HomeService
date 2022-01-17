35d7d7e2de119e029a5bdd202d68f8311642e068
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-17 18:18:27 +0330

    create ExpertControllerTest class in test package

---
+++ src/test/java/ir/farhanizade/homeservice/controller/ExpertControllerTest.java
@@ -0,0 +1,64 @@
+package ir.farhanizade.homeservice.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpertControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void addService() {
+    }
+
+    @Test
+    void suggest() {
+    }
+
+    @Test
+    void getSuggestions() {
+    }
+
+    @Test
+    void answerSuggestion() {
+    }
+
+    @Test
+    void startToWork() {
+    }
+
+    @Test
+    void finishWork() {
+    }
+
+    @Test
+    void showTransactions() {
+    }
+
+    @Test
+    void showTransaction() {
+    }
+
+    @Test
+    void showComments() {
+    }
+
+    @Test
+    void showComment() {
+    }
+
+    @Test
+    void showOrders() {
+    }
+
+    @Test
+    void showCredit() {
+    }
+
+    @Test
+    void addCredit() {
+    }
+}
