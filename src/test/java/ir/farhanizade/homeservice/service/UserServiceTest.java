4eef56bf2939a9da5ae6a10b151dc686c74eb25e
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-18 18:10:51 +0330

    create UserServiceTest class in test package

---
+++ src/test/java/ir/farhanizade/homeservice/service/UserServiceTest.java
@@ -0,0 +1,52 @@
+package ir.farhanizade.homeservice.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @Test
+    void changePassword() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void search() {
+    }
+
+    @Test
+    void getTransactions() {
+    }
+
+    @Test
+    void searchUser() {
+    }
+
+    @Test
+    void exists() {
+    }
+
+    @Test
+    void getTransaction() {
+    }
+
+    @Test
+    void getComments() {
+    }
+
+    @Test
+    void loadCreditById() {
+    }
+
+    @Test
+    void increaseCredit() {
+    }
+
+    @Test
+    void findById() {
+    }
+}
