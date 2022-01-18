aef8146add871d5da09e0299d860dec66b07eb08
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-18 20:11:02 +0330

    add @AllArgsConstructor and @NoArgsConstructor to ExpertAddSuggestionOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/ExpertAddSuggestionOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/ExpertAddSuggestionOutDto.java
@@ -1,10 +1,14 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ExpertAddSuggestionOutDto {
     private Long expertId;
     private Long orderId;
