567b23ed83fca9c5336714e0e756582d3e20d4a6
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-18 21:33:49 +0330

    create AdminServiceTest class in test package

---
+++ src/test/java/ir/farhanizade/homeservice/service/AdminServiceTest.java
@@ -0,0 +1,24 @@
+package ir.farhanizade.homeservice.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminServiceTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void finalCheck() {
+    }
+
+    @Test
+    void acceptExpert() {
+    }
+
+    @Test
+    void exists() {
+    }
+}
