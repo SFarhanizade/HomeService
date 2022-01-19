911acceebc6f4dd60cf10aff2d1b4ac21f395783
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-19 16:56:35 +0330

    add getNumberOfCustomersByRegisterTime and getNumberOfExpertsByRegisterTime methods to UserRepository interface

--- src/main/java/ir/farhanizade/homeservice/repository/user/UserRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/user/UserRepository.java
@@ -2,7 +2,16 @@ package ir.farhanizade.homeservice.repository.user;
 
 import ir.farhanizade.homeservice.entity.user.User;
 import ir.farhanizade.homeservice.repository.BaseRepository;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Date;
 
 public interface UserRepository extends BaseRepository<User>, CustomUserRepository {
 
+    @Query("select Count(c) From Customer c where c.createdTime between :time1 and :time2")
+    Long getNumberOfCustomersByRegisterTime(Date time1, Date time2, Pageable pageable);
+
+    @Query("select Count(e) From Expert e where e.createdTime between :time1 and :time2")
+    Long getNumberOfExpertsByRegisterTime(Date time1, Date time2, Pageable pageable);
 }
