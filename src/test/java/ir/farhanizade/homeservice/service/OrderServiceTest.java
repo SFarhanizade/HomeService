954b125e4d63d8d5d55c1a74474b7e97716518d7
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-19 11:01:54 +0330

    create OrderServiceTest class in test package

---
+++ src/test/java/ir/farhanizade/homeservice/service/OrderServiceTest.java
@@ -0,0 +1,52 @@
+package ir.farhanizade.homeservice.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceTest {
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void loadByExpertises() {
+    }
+
+    @Test
+    void loadAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByIdAndCustomerId() {
+    }
+
+    @Test
+    void findAllByCustomerId() {
+    }
+
+    @Test
+    void removeOrderByIdAndOwnerId() {
+    }
+
+    @Test
+    void acceptSuggestion() {
+    }
+
+    @Test
+    void findAllByExpertId() {
+    }
+
+    @Test
+    void findOrdersByCustomer() {
+    }
+
+    @Test
+    void getOrdersOfExpert() {
+    }
+}
