0507facf576291e4e88c474dfbc76ee031710a0a
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-19 09:35:36 +0330

    add required fields to OrderOfUserOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/OrderOfUserOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/OrderOfUserOutDto.java
@@ -1,4 +1,22 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderOfUserOutDto {
+    private Long userId;
+    private Long orderId;
+    private String mainService;
+    private String subService;
+    private Date createdTime;
+    private Date finishTime;
+    private Long price;
 }
