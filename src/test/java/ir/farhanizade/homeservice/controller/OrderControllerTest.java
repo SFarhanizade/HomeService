8e31e3738af37e936cc7d914a2ea104f2d07cd7a
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-19 13:01:50 +0330

    add test_get_orders_by_status_is_ok to OrderControllerTest class

--- src/test/java/ir/farhanizade/homeservice/controller/OrderControllerTest.java
+++ src/test/java/ir/farhanizade/homeservice/controller/OrderControllerTest.java
@@ -55,7 +55,7 @@ class OrderControllerTest extends AbstractRestControllerTest {
     }
 
     @Test
-    void test_get_orders_by_range_of_time() throws Exception {
+    void test_get_orders_by_range_of_time_is_ok() throws Exception {
 
         TimeRangeInDto request = new TimeRangeInDto(new Date(System.currentTimeMillis()),
                 new Date(System.currentTimeMillis()));
@@ -71,6 +71,22 @@ class OrderControllerTest extends AbstractRestControllerTest {
         mvc.perform(get("/orders/time")
                         .content(toJson(request))
                         .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.data", hasSize(2)));
+    }
+
+    @Test
+    void test_get_orders_by_status_is_ok() throws Exception {
+
+        CustomPage<OrderOfUserOutDto> result = CustomPage.<OrderOfUserOutDto>builder()
+                .data(List.of(new OrderOfUserOutDto(),
+                        new OrderOfUserOutDto()))
+                .build();
+
+        Mockito.when(orderService.getOrdersByStatus(notNull(), notNull()))
+                .thenReturn(result);
+
+        mvc.perform(get("/orders/status/0"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data.data", hasSize(2)));
     }
