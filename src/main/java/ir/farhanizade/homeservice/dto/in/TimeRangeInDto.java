9e88776a9f6c3c2c532eb638bcce56cd538d2377
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-19 11:50:27 +0330

    add required fields to TimeRangeInDto class

--- src/main/java/ir/farhanizade/homeservice/dto/in/TimeRangeInDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/in/TimeRangeInDto.java
@@ -1,4 +1,15 @@
 package ir.farhanizade.homeservice.dto.in;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TimeRangeInDto {
+    private Date time1;
+    private Date time2;
 }
