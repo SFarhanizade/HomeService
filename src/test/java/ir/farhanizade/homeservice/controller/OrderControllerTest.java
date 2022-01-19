7062d209755e636069002c56a12e9ead88c4fceb
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-19 11:57:37 +0330

    add test_get_orders_by_range_of_time test to OrderControllerTest class

--- src/test/java/ir/farhanizade/homeservice/controller/OrderControllerTest.java
+++ src/test/java/ir/farhanizade/homeservice/controller/OrderControllerTest.java
@@ -1,9 +1,12 @@
 package ir.farhanizade.homeservice.controller;
 
 import ir.farhanizade.homeservice.dto.in.ExpertInDto;
+import ir.farhanizade.homeservice.dto.in.TimeRangeInDto;
+import ir.farhanizade.homeservice.dto.out.OrderOfUserOutDto;
 import ir.farhanizade.homeservice.dto.out.OrderOutDto;
 import ir.farhanizade.homeservice.entity.CustomPage;
 import ir.farhanizade.homeservice.service.ExpertService;
+import ir.farhanizade.homeservice.service.OrderService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
@@ -13,10 +16,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.notNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,10 +32,13 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class OrderControllerTest extends AbstractRestControllerTest {
 
     @MockBean
+    private OrderService orderService;
+
+    @MockBean
     private ExpertService expertService;
 
     @Test
-    void showList() throws Exception {
+    void test_show_list_is_ok() throws Exception {
         ExpertInDto request = new ExpertInDto();
 
         CustomPage<OrderOutDto> result = CustomPage.<OrderOutDto>builder()
@@ -43,5 +52,26 @@ class OrderControllerTest extends AbstractRestControllerTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.data.data", hasSize(3)));
+    }
+
+    @Test
+    void test_get_orders_by_range_of_time() throws Exception {
+
+        TimeRangeInDto request = new TimeRangeInDto(new Date(System.currentTimeMillis()),
+                new Date(System.currentTimeMillis()));
+
+        CustomPage<OrderOfUserOutDto> result = CustomPage.<OrderOfUserOutDto>builder()
+                .data(List.of(new OrderOfUserOutDto(),
+                        new OrderOfUserOutDto()))
+                .build();
+
+        Mockito.when(orderService.getOrdersByRangeOfTime(notNull(), notNull(), notNull()))
+                .thenReturn(result);
+
+        mvc.perform(get("/orders/time")
+                        .content(toJson(request))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.data", hasSize(2)));
     }
 }
