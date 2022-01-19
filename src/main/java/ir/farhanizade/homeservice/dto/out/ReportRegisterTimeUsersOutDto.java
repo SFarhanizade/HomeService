27c63611ba835bb2344f43f11749db18256bc9cd
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-19 16:58:37 +0330

    add required fields to ReportRegisterTimeUsersOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/ReportRegisterTimeUsersOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/ReportRegisterTimeUsersOutDto.java
@@ -1,4 +1,11 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ReportRegisterTimeUsersOutDto {
+    private Long experts;
+    private Long customers;
 }
