636acc92312ab855969392f4a82d9b1bbbd06cb2
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-26 14:48:22 +0330

    make BaseRepository extend JpaSpecificationExecutor interface

--- src/main/java/ir/farhanizade/homeservice/repository/BaseRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/BaseRepository.java
@@ -2,6 +2,7 @@ package ir.farhanizade.homeservice.repository;
 
 import ir.farhanizade.homeservice.entity.core.BaseEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface BaseRepository<T extends BaseEntity> extends JpaRepository<T,Long> {
+public interface BaseRepository<T extends BaseEntity> extends JpaRepository<T, Long>, JpaSpecificationExecutor<T> {
 }
