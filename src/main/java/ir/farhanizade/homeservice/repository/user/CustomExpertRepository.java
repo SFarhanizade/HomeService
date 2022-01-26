c4bc3918d029f83af73d6a7cf84ec116d663ef75
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-26 16:11:08 +0330

    remove search method

--- src/main/java/ir/farhanizade/homeservice/repository/user/CustomExpertRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/user/CustomExpertRepository.java
@@ -8,5 +8,4 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface CustomExpertRepository {
-    CustomPage<Expert> search(UserSearchInDto user, Pageable pageable);
 }
--- src/main/java/ir/farhanizade/homeservice/repository/user/CustomUserRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/user/CustomUserRepository.java
@@ -8,5 +8,4 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface CustomUserRepository {
-    CustomPage<User> search(UserSearchInDto user, Pageable pageable);
 }
