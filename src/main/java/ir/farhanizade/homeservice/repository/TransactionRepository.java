fc36475de2ee9239629a37c4f7c42828f3131b73
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-28 23:23:36 +0330

    add findByIdAndOwnerId method to TransactionRepository interface

--- src/main/java/ir/farhanizade/homeservice/repository/TransactionRepository.java
+++ src/main/java/ir/farhanizade/homeservice/repository/TransactionRepository.java
@@ -5,7 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 
-public interface TransactionRepository extends BaseRepository<Transaction>{
+import java.util.Optional;
+
+public interface TransactionRepository extends BaseRepository<Transaction> {
     @Query("From Transaction t where t.payer.id=:id or t.recipient.id=:id")
     Page<Transaction> findByUserId(Long id, Pageable pageable);
+
+    @Query("From Transaction t where t.id=:transaction and (t.payer.id=:id or t.recipient.id=:id)")
+    Optional<Transaction> findByIdAndOwnerId(Long transaction, Long id);
 }
