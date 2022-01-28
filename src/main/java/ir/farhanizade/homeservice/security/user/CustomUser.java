7408ba681248b159ca72e1a8efa9ea74fba4441a
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-28 23:33:17 +0330

    create CustomUser class in security.user package

---
+++ src/main/java/ir/farhanizade/homeservice/security/user/CustomUser.java
@@ -0,0 +1,58 @@
+package ir.farhanizade.homeservice.security.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+@Builder
+@Data
+@AllArgsConstructor
+public class CustomUser implements UserDetails {
+    private Set<? extends GrantedAuthority> permissions;
+    private String password;
+    private String username;
+    private boolean isAccountNonExpired;
+    private boolean isAccountNonLocked;
+    private boolean isCredentialsNonExpired;
+    private boolean isEnabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return permissions;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isAccountNonExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isAccountNonLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isCredentialsNonExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+}
