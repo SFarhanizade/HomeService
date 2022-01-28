cbf29b9d9b74e04a8c29c9352e24ea3b6a846463
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-28 23:20:12 +0330

    create ApplicationSecurityConfig in security package

---
+++ src/main/java/ir/farhanizade/homeservice/security/ApplicationSecurityConfig.java
@@ -0,0 +1,73 @@
+package ir.farhanizade.homeservice.security;
+
+import ir.farhanizade.homeservice.entity.user.User;
+import ir.farhanizade.homeservice.security.user.LoggedInUser;
+import ir.farhanizade.homeservice.security.user.UserManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+    private final UserManager userService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                //.antMatchers("/users/**").hasRole(ApplicationUserRole.ADMIN.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(userService);
+        return provider;
+    }
+
+//    @Override
+//    @Bean
+//    protected UserDetailsService userDetailsService() {
+//        UserDetails annaSmith = User.builder()
+//                .username("annasmith")
+//                .password(passwordEncoder.encode("password"))
+//                .roles(ApplicationUserRole.CUSTOMER.name())
+//                .build();
+//
+//        UserDetails admin = User.builder()
+//                .username("admin")
+//                .password(passwordEncoder.encode("password"))
+//                .roles(ApplicationUserRole.ADMIN.name())
+//                .build();
+//        return new InMemoryUserDetailsManager(annaSmith, admin);
+//    }
+}
