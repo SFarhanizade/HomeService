7cfb33d2f225219cadb9105da2dcb8df6a41f05c
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-29 23:17:58 +0330

    enhance uuid creation process to create unique uuid

--- src/main/java/ir/farhanizade/homeservice/security/user/UserUUID.java
+++ src/main/java/ir/farhanizade/homeservice/security/user/UserUUID.java
@@ -2,22 +2,42 @@ package ir.farhanizade.homeservice.security.user;
 
 import ir.farhanizade.homeservice.exception.UUIDNotFoundException;
 
-import java.nio.ByteBuffer;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class UserUUID {
-    private static ConcurrentHashMap<UUID,Long> userUUID = new ConcurrentHashMap<>();
+    private static ConcurrentHashMap<String, Long> userUUID = new ConcurrentHashMap<>();
 
-    public static Long getIdByUUID(UUID uuid) throws UUIDNotFoundException {
+    public static Long getIdByUUID(String uuid) throws UUIDNotFoundException {
         Long id = userUUID.get(uuid);
-        if (id==null) throw new UUIDNotFoundException("UUID Not Found!");
+        if (id == null) throw new UUIDNotFoundException("UUID Not Found!");
+        removeUUID(uuid);
         return id;
     }
 
-    public static UUID createUUID(Long id){
-        UUID uuid = UUID.randomUUID();
-        userUUID.put(uuid,id);
+    public static String createUUID(Long id) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest salt = MessageDigest.getInstance("SHA-256");
+        salt.update(UUID.randomUUID().toString().getBytes("UTF-8"));
+        String uuid = bytesToHex(salt.digest());
+        userUUID.put(uuid, id);
         return uuid;
+    }
+
+    private static void removeUUID(String uuid) {
+        userUUID.remove(uuid);
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        StringBuilder result = new StringBuilder();
+        for (byte num : bytes) {
+            char[] hexDigits = new char[2];
+            hexDigits[0] = Character.forDigit((num >> 4) & 0xF, 16);
+            hexDigits[1] = Character.forDigit((num & 0xF), 16);
+            result.append(new String(hexDigits));
+        }
+        return result.toString();
     }
 }
