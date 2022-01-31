070a454f03a128d340435c4ae10784821dafaadb
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-01-31 20:59:59 +0330

    remove @Accepted and move the logic to LoggedInUser class

--- src/main/java/ir/farhanizade/homeservice/security/user/Accepted.java
+++
@@ -1,4 +0,0 @@
-package ir.farhanizade.homeservice.security.user;
-
-public @interface Accepted {
-}
--- src/main/java/ir/farhanizade/homeservice/security/user/LoggedInUser.java
+++ src/main/java/ir/farhanizade/homeservice/security/user/LoggedInUser.java
@@ -1,6 +1,7 @@
 package ir.farhanizade.homeservice.security.user;
 
 import ir.farhanizade.homeservice.entity.user.UserStatus;
+import ir.farhanizade.homeservice.exception.AccountIsLockedException;
 import ir.farhanizade.homeservice.exception.BadEntryException;
 import ir.farhanizade.homeservice.exception.EntityNotFoundException;
 import ir.farhanizade.homeservice.exception.UserNotLoggedInException;
@@ -10,8 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
+import java.util.Collection;
 
 @Component
 public class LoggedInUser {
@@ -26,24 +28,29 @@ public class LoggedInUser {
         service = userService;
     }
 
-    public static UserDetails get() throws UserNotLoggedInException {
+    public static UserPrincipalAndAuthorities get() throws UserNotLoggedInException, AccountIsLockedException, EntityNotFoundException {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null)
             throw new UserNotLoggedInException("User Not Logged In!");
+
         Object principal = authentication.getPrincipal();
-        if (principal instanceof UserDetails) {
-            return ((UserDetails) principal);
-        } else {
-            throw new UserNotLoggedInException("User Not Logged In!");
-        }
+        if (principal == null) throw new UserNotLoggedInException("User Not Logged In!");
+
+        UserStatus status = service.getStatusByUsername(principal.toString());
+        if (!UserStatus.ACCEPTED.equals(status))
+            throw new AccountIsLockedException("Account is Locked!");
+
+            Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+
+        return new UserPrincipalAndAuthorities(principal.toString(), authorities);
     }
 
-    public static Long id() throws UserNotLoggedInException, EntityNotFoundException, BadEntryException {
+    public static Long id() throws UserNotLoggedInException, EntityNotFoundException, BadEntryException, AccountIsLockedException {
         String username = get().getUsername();
         return service.convertUsername2Id(username);
     }
 
-    public static UserTypeAndId getTypeAndId() throws UserNotLoggedInException, BadEntryException, EntityNotFoundException {
+    public static UserTypeAndId getTypeAndId() throws UserNotLoggedInException, BadEntryException, EntityNotFoundException, AccountIsLockedException {
         String roleStr = get().getAuthorities().stream()
                 .filter(a -> a.getAuthority().startsWith("ROLE_"))
                 .findFirst()
@@ -54,7 +61,7 @@ public class LoggedInUser {
         return new UserTypeAndId(role, id());
     }
 
-    public static UserStatus getStatus() throws UserNotLoggedInException, BadEntryException, EntityNotFoundException {
+    public static UserStatus getStatus() throws UserNotLoggedInException, BadEntryException, EntityNotFoundException, AccountIsLockedException {
         Long id = id();
         return service.getStatusById(id);
     }
