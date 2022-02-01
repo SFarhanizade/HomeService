d643efdd00bce2808c9c25dc487cc9c2ef0f3eed
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-02-01 23:56:10 +0330

    add price to ServiceOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/ServiceOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/ServiceOutDto.java
@@ -3,9 +3,12 @@ package ir.farhanizade.homeservice.dto.out;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 @AllArgsConstructor
 public class ServiceOutDto {
     private Long id;
     private String name;
+    private BigDecimal price;
 }
