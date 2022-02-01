b9b13fa6a2a93f134e691e6c95486895ea495c55
SFarhanizade <sajjadfarahani94.sf@gmail.com>
2022-02-01 23:55:50 +0330

    add @AllArgsConstructor to MainServiceOutDto class

--- src/main/java/ir/farhanizade/homeservice/dto/out/MainServiceOutDto.java
+++ src/main/java/ir/farhanizade/homeservice/dto/out/MainServiceOutDto.java
@@ -1,5 +1,6 @@
 package ir.farhanizade.homeservice.dto.out;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,6 +8,7 @@ import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class MainServiceOutDto {
     private Long id;
     private String name;
